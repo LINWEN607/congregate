18b9ab97d7dd1b227f523ec15057550f63bf1e65
Michael Leopard <mleopard@gitlab.com>
2018-07-18 14:43:44 -0400

    Remove commented out code

--- stage_projects.py
+++ stage_projects.py
@@ -68,44 +68,4 @@ if __name__ == "__main__":
         with open("%s/data/stage.json" % app_path, "wb") as f:
             f.write(json.dumps(staging, indent=4))
 
-'''
-#!/bin/bash
-
-## Checking for cached project file
-if [ ! -f project_json.json ]; then
-    ${CONGREGATE_PATH}/list_projects.sh > /dev/null
-fi
-
-projects=$(cat ${CONGREGATE_PATH}/data/project_json.json)
-
-if [ "$1" = "all" ] || [ "$1" = "." ]; then
-    staging=$(jq -n '[]')
-    for ((i=0;i<`echo $projects | jq '. | length'`;i++));
-    do
-        id=$(echo $projects | jq -r ".[$i] .id")
-        name=$(echo $projects | jq -r ".[$i] .name")
-        namespace=$(echo $projects | jq -r ".[$i] .path_with_namespace" | cut -d/ -f 1)
-        staging=$(echo $staging | jq --arg name "$name" --arg id "$id" --arg namespace "$namespace" '. += [{"id": $id, "name": $name, "namespace": $namespace}]')
-    done
-else
-    staging=$(jq -n '[]')
-    for project in $@
-    do
-        if  [ "$project" -eq "$project" ] 2> /dev/null; then
-            project=$(echo $projects | jq --argjson i "$project" '.[] | select(.id == $i)')
-        else
-            project=$(echo $projects | jq --arg i "$project" '.[] | select(.name == $i)')
-        fi
-        id=$(echo $project | jq -r ".id")
-        name=$(echo $project | jq -r ".name")
-        namespace=$(echo $project | jq -r ".path_with_namespace" | cut -d/ -f 1)
-        visibility=$(echo $project | jq -r ".visibility")
-        staging=$(echo $staging | jq --arg name "$name" --arg id "$id" --arg namespace "$namespace" '. += [{"id": $id, "name": $name, "namespace": $namespace}]')
-    done
-fi
-
-if [[ ! -z $staging ]]; then
-    echo $staging | jq . > ${CONGREGATE_PATH}/data/stage.json
-fi
-
-'''
+            
