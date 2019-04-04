6a1f2736d8496fe40b53d0d0951780b085668250
Michael Leopard <mleopard@gitlab.com>
2019-04-04 09:22:30 -0400

    Adding threads setter. Updating property name in config. Updating tests. Adding threads option to congregate.py.

--- cli/config.py
+++ cli/config.py
@@ -85,7 +85,7 @@ def generate_config():
             command = "aws configure set aws_secret_access_key %s" % secret_key
             subprocess.call(command.split(" "))
 
-    config["number-of-threads"] = 2
+    config["number_of_threads"] = 2
 
     wrapper["config"] = config
     return wrapper
