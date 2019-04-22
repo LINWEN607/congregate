9f06a196cfba74c064dc055366bf4ce33890fe33
Michael Leopard <mleopard@gitlab.com>
2019-04-22 10:10:48 -0400

    Removing 'other' import from congregate.py and cp command from build-tar.sh

--- build-tar.sh
+++ build-tar.sh
@@ -6,7 +6,6 @@ cp -r helpers congregate-${CI_COMMIT_TAG}
 cp -r migration congregate-${CI_COMMIT_TAG}
 cp -r js-packages congregate-${CI_COMMIT_TAG}
 cp -r ui congregate-${CI_COMMIT_TAG}
-cp -r other congregate-${CI_COMMIT_TAG}
 cp *.py congregate-${CI_COMMIT_TAG}
 cp congregate congregate-${CI_COMMIT_TAG}
 cp Pipfile* congregate-${CI_COMMIT_TAG}
