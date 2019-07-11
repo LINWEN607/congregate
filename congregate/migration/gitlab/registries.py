e771dc684d7681b4c09ebd06bcfa6f6d9c877f87
Michael Leopard <mleopard@gitlab.com>
2019-07-11 14:33:55 -0400

    Adding json conversion and more specific API call

--- congregate/migration/gitlab/registries.py
+++ congregate/migration/gitlab/registries.py
@@ -12,14 +12,17 @@ class RegistryClient(BaseClass):
         self.projects = ProjectsClient()
         super(RegistryClient, self).__init__()
 
-    def enabled(self):
-        src = self.__enabled(self.config.child_host, self.config.child_token)
+    def enabled(self, new_id, old_id):
+        src = self.__enabled(self.config.child_host, self.config.child_token, old_id)
         dest = self.__enabled(self.config.parent_host,
-                              self.config.parent_token)
+                              self.config.parent_token, old_id)
         return src and dest
 
-    def __enabled(self, host, token):
-        return api.generate_get_request(host, token, "projects")["container_registry_enabled"]
+    def __enabled(self, host, token, id):
+        project = api.generate_get_request(host, token, "projects/%d" % id).json()
+        if project.get("container_registry_enabled"):
+            return project["container_registry_enabled"]
+        return False
 
     # Get a list of registry repositories in a project
     def __list_registry_repositories(self, host, token, id):
