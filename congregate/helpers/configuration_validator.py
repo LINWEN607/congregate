bc38d1948613c059fb87a6dbd06c1d90ed3bbb39
Michael Leopard <mleopard@gitlab.com>
2019-08-02 08:27:33 -0400

    Added validator for import user

--- congregate/helpers/configuration_validator.py
+++ congregate/helpers/configuration_validator.py
@@ -1,6 +1,7 @@
 from congregate.helpers.exceptions import ConfigurationException
 from congregate.helpers.conf import ig
 from congregate.migration.gitlab.api.groups import GroupsApi
+from congregate.migration.gitlab.api.users import UsersApi
 
 class ConfigurationValidator(ig):
     '''
@@ -8,7 +9,9 @@ class ConfigurationValidator(ig):
     '''
     def __init__(self):
         self.groups = GroupsApi()
+        self.users = UsersApi()
         self.parent_id_validated_in_session = False
+        self.parent_user_id_validated_in_session = False
         super(ConfigurationValidator, self).__init__()
 
     @property
@@ -20,6 +23,15 @@ class ConfigurationValidator(ig):
             self.parent_id_validated_in_session = self.validate_parent_group_id(parent_id)
             return parent_id
 
+    @property
+    def parent_user_id(self):
+        parent_user_id = self.config.get("parent_user_id", None)
+        if self.parent_user_id_validated_in_session is True:
+            return parent_user_id
+        else:
+            self.parent_user_id_validated_in_session = self.validate_parent_user_id(parent_user_id)
+            return parent_user_id
+
     def validate_parent_group_id(self, id):
         if id is not None:
             group_resp = self.groups.get_group(id, self.parent_host, self.parent_token).json()
@@ -29,3 +41,21 @@ class ConfigurationValidator(ig):
                 return True
         else:
             return True
+
+    def validate_parent_user_id(self, id):
+        if id is None:
+            raise ConfigurationException("parent_user_id")
+        else:
+            user_resp = self.users.get_current_user(self.parent_host, self.parent_token).json()
+            if user_resp.get("message", None) is not None:
+                raise ConfigurationException("parent_user_id")
+            elif user_resp.get("error", None) is not None:
+                if user_resp["error"] == "invalid_token":
+                    raise ConfigurationException("parent_token")
+                else:
+                    raise Exception
+            else:
+                if user_resp["id"] == id:
+                    return True
+                else:
+                    raise ConfigurationException("parent_user_id")
--- congregate/tests/helpers/test_configuration_validator.py
+++ congregate/tests/helpers/test_configuration_validator.py
@@ -5,6 +5,9 @@ import responses
 import json
 from congregate.helpers.configuration_validator import ConfigurationValidator
 from congregate.helpers.mockapi.groups import MockGroupsApi
+from congregate.helpers.mockapi.users import MockUsersApi
+from congregate.helpers.mockapi.token import invalid_token
+from congregate.helpers.mockapi.error import other_error
 from congregate.helpers.exceptions import ConfigurationException
 from congregate.helpers.api import generate_v4_request_url
 
@@ -13,6 +16,7 @@ class ConfigurationValidationTests(unittest.TestCase):
     # pylint: disable=no-member
     def setUp(self):
         self.groups = MockGroupsApi()
+        self.users = MockUsersApi()
         self.config = ConfigurationValidator()
 
     @responses.activate
@@ -39,5 +43,72 @@ class ConfigurationValidationTests(unittest.TestCase):
                   json=self.groups.get_group(), status=200, content_type='text/json', match_querystring=True)
         # pylint: enable=no-member
         self.assertTrue(self.config.validate_parent_group_id(4))
+
+    @responses.activate
+    # pylint: enable=no-member
+    @mock.patch("congregate.helpers.api.generate_v4_request_url")
+    def test_fail_parent_user_id_validation(self, url):
+        url_value = "https://gitlab.com/api/v4/users"
+        url.return_value = url_value
+        # pylint: disable=no-member
+        responses.add(responses.GET, url_value,
+                  json=self.users.get_user_404(), status=404, content_type='text/json', match_querystring=True)
+        # pylint: enable=no-member
+        # self.assertRaises(ConfigurationException, self.config.validate_parent_user_id, 1)
+        with self.assertRaises(ConfigurationException) as context:
+            self.config.parent_user_id
+
+        self.assertTrue(context.exception)
+
+    def test_no_parent_user_id_validation(self):
+        self.assertRaises(ConfigurationException, self.config.validate_parent_user_id, None)
+
+    @responses.activate
+    # pylint: enable=no-member
+    @mock.patch("congregate.helpers.api.generate_v4_request_url")
+    def test_invalid_token(self, url):
+        url_value = "https://gitlab.com/api/v4/users"
+        url.return_value = url_value
+        # pylint: disable=no-member
+        responses.add(responses.GET, url_value,
+                  json=invalid_token(), status=404, content_type='text/json', match_querystring=True)
+        # pylint: enable=no-member
+        self.assertRaises(ConfigurationException, self.config.validate_parent_user_id, 1)
+
+    @responses.activate
+    # pylint: enable=no-member
+    @mock.patch("congregate.helpers.api.generate_v4_request_url")
+    def test_unexpected_error(self, url):
+        url_value = "https://gitlab.com/api/v4/users"
+        url.return_value = url_value
+        # pylint: disable=no-member
+        responses.add(responses.GET, url_value,
+                  json=other_error(), status=404, content_type='text/json', match_querystring=True)
+        # pylint: enable=no-member
+        self.assertRaises(Exception, self.config.validate_parent_user_id, 1)
+
+    @responses.activate
+    # pylint: enable=no-member
+    @mock.patch("congregate.helpers.api.generate_v4_request_url")
+    def test_pass_parent_user_validation(self, url):
+        url_value = "https://gitlab.com/api/v4/users"
+        url.return_value = url_value
+        # pylint: disable=no-member
+        responses.add(responses.GET, url_value,
+                  json=self.users.get_current_user(), status=200, content_type='text/json', match_querystring=True)
+        # pylint: enable=no-member
+        self.assertTrue(self.config.validate_parent_user_id(1))
+
+    @responses.activate
+    # pylint: enable=no-member
+    @mock.patch("congregate.helpers.api.generate_v4_request_url")
+    def test_mismatched_parent_user_id_validation(self, url):
+        url_value = "https://gitlab.com/api/v4/users"
+        url.return_value = url_value
+        # pylint: disable=no-member
+        responses.add(responses.GET, url_value,
+                  json=self.users.get_current_user(), status=200, content_type='text/json', match_querystring=True)
+        # pylint: enable=no-member
+        self.assertRaises(ConfigurationException, self.config.validate_parent_user_id, 2)
 
     
