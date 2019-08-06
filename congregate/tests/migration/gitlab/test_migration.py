8a4466639e9a878e7272819f1ebffd799071fe7b
Michael Leopard <mleopard@gitlab.com>
2019-08-06 14:27:34 -0400

    Attempting to cache pip deps

--- congregate/tests/migration/gitlab/test_migration.py
+++ congregate/tests/migration/gitlab/test_migration.py
@@ -5,11 +5,13 @@ from uuid import uuid4
 from congregate.helpers.misc_utils import input_generator
 from congregate.cli import config
 from congregate.helpers.seed.generate_token import token_generator
+# from congregate.helpers.seed.generator import generate_seed_data
 
 t = token_generator()
 
 @pytest.mark.e2e
 def test_gitlab_migration():
+    print os.getenv("GITLAB_SRC")
     values = [
         "",  # Migration source
         os.getenv("destination_instance_host"),
@@ -25,6 +27,9 @@ def test_gitlab_migration():
     ]
     g = input_generator(values)
 
+
     with mock.patch('__builtin__.raw_input', lambda x: next(g)):
-        print config.generate_config()
+        config.update_config(config.generate_config())
+
+    # generate_seed_data()
 
