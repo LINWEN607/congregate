3e0b1b4e55963de3eec6b1dc12e81e360672f80f
Michael Leopard <mleopard@gitlab.com>
2019-08-07 14:32:52 -0400

    Trying out another approach with the config

--- congregate/tests/migration/gitlab/test_migration.py
+++ congregate/tests/migration/gitlab/test_migration.py
@@ -1,6 +1,7 @@
 import pytest
 import mock
 import os
+import json
 from uuid import uuid4
 from congregate.helpers.misc_utils import input_generator
 from congregate.cli import config
@@ -12,25 +13,44 @@ t = token_generator()
 @pytest.mark.e2e
 def test_gitlab_migration():
     print os.getenv("GITLAB_SRC")
-    values = [
-        "",  # Migration source
-        os.getenv("destination_instance_host"),
-        os.getenv("destination_instance_token"),
-        "http://gitlab_source",
-        t.generate_token("source_token", "2020-08-27", url="http://gitlab_source", username="root", pword=uuid4().hex),
-        os.getenv("source_instance_registry"),
-        os.getenv("destination_instance_registry"),
-        "",  # Parent group id
-        "",  # Mirroring yes/no
-        "",  # Staging location (default filesystem)
-        ""  # Staging location path
-    ]
-    g = input_generator(values)
+    # values = [
+    #     "",  # Migration source
+    #     os.getenv("destination_instance_host"),
+    #     os.getenv("destination_instance_token"),
+    #     "http://gitlab_source",
+    #     ,
+    #     os.getenv("source_instance_registry"),
+    #     os.getenv("destination_instance_registry"),
+    #     "",  # Parent group id
+    #     "",  # Mirroring yes/no
+    #     "",  # Staging location (default filesystem)
+    #     ""  # Staging location path
+    # ]
+    # g = input_generator(values)
 
+    test_config = {
+        "config": {
+            "external_source": False,
+            "source_instance_host": os.getenv("GITLAB_SRC"),
+            "source_instance_token": t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex),
+            "source_instance_registry": os.getenv("source_instance_registry"),
+            "destination_instance_host": os.getenv("GITLAB_DEST"),
+            "destination_instance_token": t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex),
+            "destination_instance_registry": os.getenv("destination_instance_registry"),
+            "number_of_threads": 2,
+            "location": "filesystem",
+            "import_user_id": 1,
+            "path": os.getcwd()
+        }
+    }
 
-    with mock.patch('__builtin__.raw_input', lambda x: next(g)):
-        # config.update_config(config.generate_config())
-        print config.generate_config()
+    print json.dumps(test_config, indent=4)
+
+    config.update_config(test_config)
+
+    # with mock.patch('__builtin__.raw_input', lambda x: next(g)):
+    #     # config.update_config(config.generate_config())
+    #     print config.generate_config()
 
     # generate_seed_data()
 
