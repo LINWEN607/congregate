974f114f464c5db353a8586e8b2042d7a57366e0
Michael Leopard <mleopard@gitlab.com>
2019-08-08 08:13:43 -0400

    Changing seed data to class and e2e test to class

--- congregate/helpers/seed/generator.py
+++ congregate/helpers/seed/generator.py
@@ -1,4 +1,4 @@
-from congregate.helpers import base_module as b
+from congregate.helpers.base_class import BaseClass
 from congregate.migration.gitlab.importexport import ImportExportClient
 from congregate.migration.gitlab.variables import VariablesClient
 from congregate.migration.gitlab.users import UsersClient
@@ -12,147 +12,150 @@ from congregate.migration.gitlab.registries import RegistryClient
 from congregate.migration.gitlab.pipeline_schedules import PipelineSchedulesClient
 from congregate.migration.mirror import MirrorClient
 from congregate.migration.gitlab.deploy_keys import DeployKeysClient
-
 from datetime import timedelta, date
 import json
 
-ie = ImportExportClient()
-mirror = MirrorClient()
-variables = VariablesClient()
-users = UsersClient()
-groups = GroupsClient()
-projects = ProjectsClient()
-pushrules = PushRulesClient()
-branches = BranchesClient()
-awards = AwardsClient()
-mr = MergeRequestApproversClient()
-registries = RegistryClient()
-schedules = PipelineSchedulesClient()
-deploy_keys = DeployKeysClient()
+class SeedDataGenerator(BaseClass):
+    def __init__(self):
+        self.ie = ImportExportClient()
+        self.mirror = MirrorClient()
+        self.variables = VariablesClient()
+        self.users = UsersClient()
+        self.groups = GroupsClient()
+        self.projects = ProjectsClient()
+        self.pushrules = PushRulesClient()
+        self.branches = BranchesClient()
+        self.awards = AwardsClient()
+        self.mr = MergeRequestApproversClient()
+        self.registries = RegistryClient()
+        self.schedules = PipelineSchedulesClient()
+        self.deploy_keys = DeployKeysClient()
 
-def generate_users():
-    dummy_users = [
-        {
-            "username": "john_smith",
-            "email": "john@example.com",
-            "name": "John Smith",
-            "reset_password": True,
-            "skip_confirmation": True
-        },
-        {
-            "username": "jack_smith",
-            "email": "jack@example.com",
-            "name": "Jack Smith",
-            "reset_password": True,
-            "skip_confirmation": True
-        },
-        {
-            "username": "jane_doe",
-            "email": "jane@example.com",
-            "name": "Jane Doe",
-            "reset_password": True,
-            "skip_confirmation": True
-        }
-    ]
-    created_users = []
-    for user in dummy_users:
-        user_search = list(users.search_for_user_by_email(b.config.source_host, b.config.source_token, user["email"]))
-        if user_search[0]["email"] == user["email"]:
-            b.log.info("%s already exists" % user["name"])
-            created_users.append(user_search[0])
-        else:
-            b.log.info("Creating %s" % user["name"])
-            created_users.append(users.create_user(b.config.source_host, b.config.source_token, user).json())
+    def generate_seed_data(self):
+        users = self.generate_users()
+        groups = self.generate_groups()
+        self.add_group_members(users, groups)
+        projects = self.generate_group_projects(groups)
+        projects += self.generate_user_projects(users)
 
-    return created_users
+        print "---Generated Users---"
+        print json.dumps(users, indent=4)
+        print "---Generated Groups---"
+        print json.dumps(groups, indent=4)
+        print "---Generated Projects---"
+        print json.dumps(projects, indent=4)
 
-def generate_groups():
-    dummy_groups = [
-        {
-            "name": "Dummy Group 1",
-            "path": "dummy-group-1"
-        },
-        {
-            "name": "Dummy Group 2",
-            "path": "dummy-group-2"
-        }
-    ]
-    created_groups = []
-    for group in dummy_groups:
-        group_search = list(groups.search_for_group(group["path"], b.config.source_host, b.config.source_token))
-        if group_search[0]["path"] == group["path"]:
-            b.log.info("%s already exists" % group["name"])
-            created_groups.append(group_search[0])
-        else:
-            b.log.info("Creating %s" % group["name"])
-            created_groups.append(groups.create_group(b.config.source_host, b.config.source_token, group).json())
-    
-    return created_groups
+    def generate_users(self):
+        dummy_users = [
+            {
+                "username": "john_smith",
+                "email": "john@example.com",
+                "name": "John Smith",
+                "reset_password": True,
+                "skip_confirmation": True
+            },
+            {
+                "username": "jack_smith",
+                "email": "jack@example.com",
+                "name": "Jack Smith",
+                "reset_password": True,
+                "skip_confirmation": True
+            },
+            {
+                "username": "jane_doe",
+                "email": "jane@example.com",
+                "name": "Jane Doe",
+                "reset_password": True,
+                "skip_confirmation": True
+            }
+        ]
+        created_users = []
+        for user in dummy_users:
+            user_search = list(self.users.search_for_user_by_email(self.config.source_host, self.config.source_token, user["email"]))
+            if user_search[0]["email"] == user["email"]:
+                self.log.info("%s already exists" % user["name"])
+                created_users.append(user_search[0])
+            else:
+                self.log.info("Creating %s" % user["name"])
+                created_users.append(self.users.create_user(self.config.source_host, self.config.source_token, user).json())
 
-def add_group_members(created_users, created_groups):
-    for user in created_users:
-        print user
-        data = {
-            "user_id": user["id"],
-            "access_level": 40
-        }
-        print created_groups[-1]
-        groups.add_member_to_group(created_groups[-1]["id"], b.config.source_host, b.config.source_token, data)
+        return created_users
+
+    def generate_groups(self):
+        dummy_groups = [
+            {
+                "name": "Dummy Group 1",
+                "path": "dummy-group-1"
+            },
+            {
+                "name": "Dummy Group 2",
+                "path": "dummy-group-2"
+            }
+        ]
+        created_groups = []
+        for group in dummy_groups:
+            group_search = list(self.groups.search_for_group(group["path"], self.config.source_host, self.config.source_token))
+            if group_search[0]["path"] == group["path"]:
+                self.log.info("%s already exists" % group["name"])
+                created_groups.append(group_search[0])
+            else:
+                self.log.info("Creating %s" % group["name"])
+                created_groups.append(self.groups.create_group(self.config.source_host, self.config.source_token, group).json())
         
-def generate_group_projects(created_groups):
-    dummy_projects = {
-        "spring": "https://gitlab.com/gitlab-org/project-templates/spring.git",
-        "react": "https://gitlab.com/gitlab-org/project-templates/react.git",
-        "android": "https://gitlab.com/gitlab-org/project-templates/android.git"
-    }
+        return created_groups
 
-    created_projects = []
+    def add_group_members(self, created_users, created_groups):
+        for user in created_users:
+            print user
+            data = {
+                "user_id": user["id"],
+                "access_level": 40
+            }
+            print created_groups[-1]
+            self.groups.add_member_to_group(created_groups[-1]["id"], self.config.source_host, self.config.source_token, data)
+            
+    def generate_group_projects(self, created_groups):
+        dummy_projects = {
+            "spring": "https://gitlab.com/gitlab-org/project-templates/spring.git",
+            "react": "https://gitlab.com/gitlab-org/project-templates/react.git",
+            "android": "https://gitlab.com/gitlab-org/project-templates/android.git"
+        }
+
+        created_projects = []
+        
+        for project_name, project_url in dummy_projects.items():
+            data = {
+                "import_url": project_url,
+                "namespace_id": created_groups[-1]["id"]
+            }
+            created_projects.append(self.projects.create_project(self.config.source_host, self.config.source_token, project_name, data).json())
+
+        return created_projects
+
+    def generate_user_projects(self, created_users):
+        dummy_project_data = [
+            {
+                "name":"my-project",
+                "import_url": "https://gitlab.com/gitlab-org/project-templates/spring.git"
+            },
+            {        
+                "name": "my-awesome-project",
+                "import_url": "https://gitlab.com/gitlab-org/project-templates/react.git",
+            }, 
+            {
+                "name": "test",
+                "import_url": "https://gitlab.com/gitlab-org/project-templates/android.git"
+            }
+        ]
+        created_projects = []
+        users_map = {}
+        expiration_date = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+        for i in range(0, len(created_users)):
+            user = created_users[i]
+            token = self.users.find_or_create_impersonation_token(self.config.source_host, self.config.source_token, user, users_map, expiration_date)["token"]
+            created_projects.append(self.projects.create_project(self.config.source_host, token, dummy_project_data[i]["name"], data=dummy_project_data[i]).json())
+
+        return created_projects
+
     
-    for project_name, project_url in dummy_projects.items():
-        data = {
-            "import_url": project_url,
-            "namespace_id": created_groups[-1]["id"]
-        }
-        created_projects.append(projects.create_project(b.config.source_host, b.config.source_token, project_name, data).json())
-
-    return created_projects
-
-def generate_user_projects(created_users):
-    dummy_project_data = [
-        {
-            "name":"my-project",
-            "import_url": "https://gitlab.com/gitlab-org/project-templates/spring.git"
-        },
-        {        
-            "name": "my-awesome-project",
-            "import_url": "https://gitlab.com/gitlab-org/project-templates/react.git",
-        }, 
-        {
-            "name": "test",
-            "import_url": "https://gitlab.com/gitlab-org/project-templates/android.git"
-        }
-    ]
-    created_projects = []
-    users_map = {}
-    expiration_date = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
-    for i in range(0, len(created_users)):
-        user = created_users[i]
-        token = users.find_or_create_impersonation_token(b.config.source_host, b.config.source_token, user, users_map, expiration_date)["token"]
-        created_projects.append(projects.create_project(b.config.source_host, token, dummy_project_data[i]["name"], data=dummy_project_data[i]).json())
-
-    return created_projects
-
-def generate_seed_data():
-    users = generate_users()
-    groups = generate_groups()
-    add_group_members(users, groups)
-    projects = generate_group_projects(groups)
-    projects += generate_user_projects(users)
-
-    print "---Generated Users---"
-    print json.dumps(users, indent=4)
-    print "---Generated Groups---"
-    print json.dumps(groups, indent=4)
-    print "---Generated Projects---"
-    print json.dumps(projects, indent=4)
 
--- congregate/tests/migration/gitlab/test_migration.py
+++ congregate/tests/migration/gitlab/test_migration.py
@@ -2,56 +2,61 @@ import pytest
 import mock
 import os
 import json
+import unittest
 from uuid import uuid4
 from congregate.helpers.misc_utils import input_generator
 from congregate.cli import config
 from congregate.helpers.seed.generate_token import token_generator
-# from congregate.helpers.seed.generator import generate_seed_data
-
-t = token_generator()
+from congregate.helpers.seed.generator import SeedDataGenerator
 
 @pytest.mark.e2e
-def test_gitlab_migration():
-    print os.getenv("GITLAB_SRC")
-    print os.getenv("GITLAB_DEST")
-    # values = [
-    #     "",  # Migration source
-    #     os.getenv("destination_instance_host"),
-    #     os.getenv("destination_instance_token"),
-    #     "http://gitlab_source",
-    #     ,
-    #     os.getenv("source_instance_registry"),
-    #     os.getenv("destination_instance_registry"),
-    #     "",  # Parent group id
-    #     "",  # Mirroring yes/no
-    #     "",  # Staging location (default filesystem)
-    #     ""  # Staging location path
-    # ]
-    # g = input_generator(values)
+class MigrationEndToEndTest(unittest.TestCase):
+    def setUp(self):
+        self.t = token_generator()
+        self.generate_default_config_with_tokens()
+        self.s = SeedDataGenerator()
 
-    test_config = {
-        "config": {
-            "external_source": False,
-            "source_instance_host": os.getenv("GITLAB_SRC"),
-            "source_instance_token": t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex),
-            "source_instance_registry": os.getenv("source_instance_registry"),
-            "destination_instance_host": os.getenv("GITLAB_DEST"),
-            "destination_instance_token": t.generate_token("destination_token", "2020-08-27", url=os.getenv("GITLAB_DEST"), username="root", pword=uuid4().hex),
-            "destination_instance_registry": os.getenv("destination_instance_registry"),
-            "number_of_threads": 2,
-            "location": "filesystem",
-            "import_user_id": 1,
-            "path": os.getcwd()
-        }
-    }
+    def test_seed_data(self):
+        self.s.generate_seed_data()
 
-    print json.dumps(test_config, indent=4)
+    def generate_default_config_with_tokens(self):
+        values = [
+            "",  # Migration source
+            os.getenv("GITLAB_DEST"),
+            self.t.generate_token("destination_token", "2020-08-27", url=os.getenv("GITLAB_DEST"), username="root", pword=uuid4().hex),
+            os.getenv("GITLAB_SRC"),
+            self.t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex),
+            os.getenv("source_instance_registry"),
+            os.getenv("destination_instance_registry"),
+            "",  # Parent group id
+            "",  # Mirroring yes/no
+            "",  # Staging location (default filesystem)
+            ""  # Staging location path
+        ]
 
-    config.update_config(test_config)
+        g = input_generator(values)
 
-    # with mock.patch('__builtin__.raw_input', lambda x: next(g)):
-    #     # config.update_config(config.generate_config())
-    #     print config.generate_config()
+        # test_config = {
+        #     "config": {
+        #         "external_source": False,
+        #         "source_instance_host": os.getenv("GITLAB_SRC"),
+        #         "source_instance_token": t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex),
+        #         "source_instance_registry": os.getenv("source_instance_registry"),
+        #         "destination_instance_host": os.getenv("GITLAB_DEST"),
+        #         "destination_instance_token": t.generate_token("destination_token", "2020-08-27", url=os.getenv("GITLAB_DEST"), username="root", pword=uuid4().hex),
+        #         "destination_instance_registry": os.getenv("destination_instance_registry"),
+        #         "number_of_threads": 2,
+        #         "location": "filesystem",
+        #         "import_user_id": 1,
+        #         "path": os.getcwd()
+        #     }
+        # }
 
-    # generate_seed_data()
+        # print json.dumps(test_config, indent=4)
+
+        # config.update_config(test_config)
+
+        with mock.patch('__builtin__.raw_input', lambda x: next(g)):
+            config.config()
+
 
