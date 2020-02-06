49f76b9f6e4ee32d0cdfc7bb7a1755b64c7194a0
Michael Leopard <mleopard@gitlab.com>
2020-02-06 07:35:36 -0500

    Adding envirnoments migration to migrate.py. Adding boolean return to migration function

--- congregate/migration/migrate.py
+++ congregate/migration/migrate.py
@@ -34,6 +34,7 @@ from congregate.migration.gitlab.project_export import ProjectExportClient
 from congregate.migration.mirror import MirrorClient
 from congregate.migration.gitlab.deploy_keys import DeployKeysClient
 from congregate.migration.gitlab.hooks import HooksClient
+from congregate.migration.gitlab.environments import EnvironmentsClient
 from congregate.migration.bitbucket import client as bitbucket
 
 aws = AwsClient()
@@ -55,6 +56,7 @@ p_schedules = PipelineSchedulesClient()
 deploy_keys = DeployKeysClient()
 hooks = HooksClient()
 project_export = ProjectExportClient()
+environments = EnvironmentsClient()
 
 full_parent_namespace = groups.find_parent_group_path()
 
@@ -380,6 +382,9 @@ def migrate_single_project_info(project, new_id):
     # Container Registries
     results["container_registry"] = registries.migrate_registries(
         old_id, new_id, name)
+
+    results["environments"] = environments.migrate_project_environments(
+        old_id, new_id)
 
     # Project hooks (webhooks)
     results["project_hooks"] = hooks.migrate_project_hooks(
