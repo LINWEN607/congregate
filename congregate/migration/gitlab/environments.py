2ac525771afc5ba289d3e2415599cd61e7181e9a
Michael Leopard <mleopard@gitlab.com>
2020-02-06 07:07:21 -0500

    adding basic unit test for envs

--- congregate/migration/gitlab/environments.py
+++ congregate/migration/gitlab/environments.py
@@ -8,7 +8,13 @@ class EnvironmentsClient(BaseClass):
 
     def migrate_project_environments(self, source_project_id, destination_project_id):
         for environment in self.environments.get_all_environments(source_project_id, self.config.source_host, self.config.source_token):
-            environment.pop("state")
-            environment.pop("slug")
-            environment.pop("id")
-            self.environments.create_environment(self.config.destination_host, self.config.destination_token, destination_project_id, environment)
+            self.environments.create_environment(self.config.destination_host, self.config.destination_token, destination_project_id, self.generate_environment_data(environment))
+
+    def handle_project_environment_migration(self, destination_project_id, environment):
+        self.environments.create_environment(self.config.destination_host, self.config.destination_token, destination_project_id, self.generate_environment_data(environment))
+
+    def generate_environment_data(self, environment):
+        environment.pop("state")
+        environment.pop("slug")
+        environment.pop("id")
+        return environment
---
+++ congregate/tests/migration/gitlab/test_environments.py
@@ -0,0 +1,15 @@
+from congregate.tests.mockapi.environments import MockEnvironmentsApi
+from congregate.migration.gitlab.environments import EnvironmentsClient
+
+mock_environment = MockEnvironmentsApi()
+environment = EnvironmentsClient()
+
+def test_generate_environment_data():
+    expected = {
+        "name": "review/fix-foo",
+        "external_url": "https://review-fix-foo-dfjre3.example.gitlab.com",
+    }
+    envs = list(mock_environment.get_all_environments_generator())
+    actual = environment.generate_environment_data(envs[0][0])
+
+    assert expected == actual
