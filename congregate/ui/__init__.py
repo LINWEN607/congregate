b285285fa50b609d87ef467825e2319de1228dd9
Michael Leopard <mleopard@gitlab.com>
2020-02-27 14:37:42 -0500

    Fixing broken UI due to autopep8. Removing "external" from user data generation.

--- congregate/ui/__init__.py
+++ congregate/ui/__init__.py
@@ -1,11 +1,10 @@
-from congregate.helpers.logger import myLogger
-import logging
-from . import controllers
-from . import models
-from . import views
-from flask import Flask
+###
+### If this file is changed due to autopep8, revert the change or else the UI won't load
+###
 import os
 import sys
+import logging
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -14,6 +13,12 @@ sys.path.append(
         os.path.dirname(
             os.path.dirname(
                 os.path.abspath(__file__)))))
+
+from . import controllers
+from . import models
+from . import views
+
+from congregate.helpers.logger import myLogger
 
 
 log = myLogger('werkzeug')
