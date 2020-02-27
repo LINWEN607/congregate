3b31d8163b6ee0cdbaf4eeaf58500c14e6c9487d
Petar Prokic <pprokic@gitlab.com>
2020-02-27 21:17:43 +0100

    Removed notification_level and strip_namespace_prefix from e2e test

--- congregate/tests/migration/gitlab/test_migration_setup.py
+++ congregate/tests/migration/gitlab/test_migration_setup.py
@@ -2,11 +2,11 @@ import os
 import unittest
 from uuid import uuid4
 import base64
+import json
 import pytest
 import mock
-import json
 from congregate.helpers.misc_utils import input_generator
-from congregate.cli import config, do_all
+from congregate.cli import config
 from congregate.helpers.seed.generate_token import token_generator
 from congregate.helpers.seed.generator import SeedDataGenerator
 
@@ -24,47 +24,47 @@ class MigrationEndToEndTestSetup(unittest.TestCase):
 
     def generate_default_config_with_tokens(self):
         print "Generating Destination Token"
-        destination_token = self.t.generate_token("destination_token", "2020-08-27", url=os.getenv("GITLAB_DEST"), username="root", pword=uuid4().hex) # Destination access token
+        destination_token = self.t.generate_token("destination_token", "2020-08-27", url=os.getenv(
+            "GITLAB_DEST"), username="root", pword=uuid4().hex)  # Destination access token
         print "Generating Source Token"
-        source_token = self.t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword="5iveL!fe") # source token
+        source_token = self.t.generate_token("source_token", "2020-08-27", url=os.getenv(
+            "GITLAB_SRC"), username="root", pword="5iveL!fe")  # source token
         print "Prepping config data"
         values = [
-            os.getenv("GITLAB_DEST"), # Destination hostname
+            os.getenv("GITLAB_DEST"),  # Destination hostname
             # self.t.generate_token("destination_token", "2020-08-27", url=os.getenv("GITLAB_DEST"), username="root", pword=uuid4().hex), # Destination access token
             # "0",  # Destination import user id
-            "True", # shared runners enabled
-            "False", # append project suffix (retry)
-            "disabled", # notification level
-            "3", # max_import_retries,
-            "gitlab", # external_src_url
-            os.getenv("GITLAB_SRC"), # source host
+            "True",  # shared runners enabled
+            "False",  # append project suffix (retry)
+            "3",  # max_import_retries,
+            "gitlab",  # external_src_url
+            os.getenv("GITLAB_SRC"),  # source host
             # self.t.generate_token("source_token", "2020-08-27", url=os.getenv("GITLAB_SRC"), username="root", pword=uuid4().hex), # source token
-            os.getenv("GITLAB_SRC_REG_URL"), # source registry url
-            "300", # max_export_wait_time
-            os.getenv("GITLAB_DEST_REG_URL"), # destination registry url
-            "", # destination parent group id
+            os.getenv("GITLAB_SRC_REG_URL"),  # source registry url
+            "300",  # max_export_wait_time
+            os.getenv("GITLAB_DEST_REG_URL"),  # destination registry url
+            "",  # destination parent group id
             # "parent_group_path",  # destination parent group full path
             # "True",  # privatize_groups
             # "group_sso_provider",  # SSO provider
             # "username_suffix",  # username suffix
-            "No", # mirror
-            "filesystem", # export location
+            "No",  # mirror
+            "filesystem",  # export location
             # "s3_name",  # bucket name
             # "us-east-1",    # bucket region
             # "access key",   # access key
             # "secret key",   # secret key
-            "", # file system path
-            "False", # keep_blocked_users
-            "True", # password reset email
-            "False", # randomized password
-            "3", # Threads
-            "True", # strip namespace prefix
-            "30" # import wait time
+            "",  # file system path
+            "False",  # keep_blocked_users
+            "True",  # password reset email
+            "False",  # randomized password
+            "3",  # Threads
+            "30"  # import wait time
         ]
         print json.dumps(values)
         tokens = [
-           destination_token,
-           source_token
+            destination_token,
+            source_token
         ]
 
         g = input_generator(values)
