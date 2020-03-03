2305ffa5068a66076047ddda4b07f1d67f4330eb
Michael Leopard <mleopard@gitlab.com>
2020-03-03 10:25:47 -0500

    formatting environments.py

--- congregate/migration/gitlab/environments.py
+++ congregate/migration/gitlab/environments.py
@@ -1,6 +1,7 @@
 from congregate.helpers.base_class import BaseClass
 from congregate.migration.gitlab.api.projects import ProjectsApi
 
+
 class EnvironmentsClient(BaseClass):
     def __init__(self):
         self.projects = ProjectsApi()
@@ -9,7 +10,8 @@ class EnvironmentsClient(BaseClass):
     def migrate_project_environments(self, source_project_id, destination_project_id):
         try:
             for environment in self.projects.get_all_environments(source_project_id, self.config.source_host, self.config.source_token):
-                self.projects.create_environment(self.config.destination_host, self.config.destination_token, destination_project_id, self.generate_environment_data(environment))
+                self.projects.create_environment(self.config.destination_host, self.config.destination_token,
+                                                 destination_project_id, self.generate_environment_data(environment))
         except Exception as e:
             self.log.error(
                 "Failed to migrate project (ID: {0}) environments, with error:\n{1}".format(source_project_id, e))
