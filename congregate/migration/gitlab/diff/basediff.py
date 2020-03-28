d7880c7ddf0dc1dd0bfdd2f9832ca38d20129d3a
Michael Leopard <mleopard@gitlab.com>
2020-03-28 09:10:40 -0400

    Attempting to fix sorting in generate_cleaned_instance_data

--- congregate/migration/gitlab/diff/basediff.py
+++ congregate/migration/gitlab/diff/basediff.py
@@ -248,16 +248,17 @@ class BaseDiffClient(BaseClass):
         if isinstance(instance_data, GeneratorType):
             try:
                 instance_data = self.ignore_keys(list(instance_data))
+                instance_data.sort()
             except TypeError:
                 self.log.error(
                     "Unable to generate cleaned instance data. Returning empty list")
                 return []
         elif isinstance(instance_data, list):
-            instance_data = self.ignore_keys(instance_data)
+            instance_data = sorted(self.ignore_keys(instance_data))
         else:
-            instance_data = self.ignore_keys(instance_data.json())
+            instance_data = sorted(self.ignore_keys(instance_data.json()))
         
-        return sorted(instance_data)
+        return instance_data
 
     def generate_empty_data(self, source):
         if isinstance(source, list):
