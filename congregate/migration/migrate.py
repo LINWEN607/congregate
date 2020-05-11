7a7a177e19b7fc74643bf83eb395e1aa29902ff3
Michael Leopard <mleopard@gitlab.com>
2020-05-11 16:45:01 -0400

    Adding additional None check

--- congregate/migration/migrate.py
+++ congregate/migration/migrate.py
@@ -233,27 +233,28 @@ def handle_importing_groups(group):
             full_path_with_parent_namespace))
         dst_group = groups.find_group_by_path(
             b.config.destination_host, b.config.destination_token, full_path_with_parent_namespace)
-        dst_gid = dst_group.get("id", None)
-        if dst_gid:
-            b.log.info("{0}Group {1} (ID: {2}) already exists on destination".format(
-                get_dry_log(_DRY_RUN), full_path, dst_gid))
-            result[full_path_with_parent_namespace] = dst_gid
-        else:
-            b.log.info("{0}Group {1} NOT found on destination, importing..."
-                       .format(get_dry_log(_DRY_RUN), full_path_with_parent_namespace))
-            ie.import_group(
-                group, full_path_with_parent_namespace, filename, dry_run=_DRY_RUN)
-            # In place of checking the import status
-            if not _DRY_RUN:
-                group = ie.wait_for_group_import(
-                    full_path_with_parent_namespace)
-                if group and group.get("id", None):
-                    result[full_path_with_parent_namespace] = group
-                    # Migrate CI/CD Variables
-                    variables.migrate_variables(
-                        group["id"], src_gid, "group", full_path)
-                    # Remove import user
-                    groups.remove_import_user(group["id"])
+        if dst_group:
+            dst_gid = dst_group.get("id", None)
+            if dst_gid:
+                b.log.info("{0}Group {1} (ID: {2}) already exists on destination".format(
+                    get_dry_log(_DRY_RUN), full_path, dst_gid))
+                result[full_path_with_parent_namespace] = dst_gid
+            else:
+                b.log.info("{0}Group {1} NOT found on destination, importing..."
+                        .format(get_dry_log(_DRY_RUN), full_path_with_parent_namespace))
+                ie.import_group(
+                    group, full_path_with_parent_namespace, filename, dry_run=_DRY_RUN)
+                # In place of checking the import status
+                if not _DRY_RUN:
+                    group = ie.wait_for_group_import(
+                        full_path_with_parent_namespace)
+                    if group and group.get("id", None):
+                        result[full_path_with_parent_namespace] = group
+                        # Migrate CI/CD Variables
+                        variables.migrate_variables(
+                            group["id"], src_gid, "group", full_path)
+                        # Remove import user
+                        groups.remove_import_user(group["id"])
     except (RequestException, KeyError, OverflowError) as oe:
         b.log.error("Failed to import group {0} (ID: {1}) as {2} with error:\n{3}".format(
             full_path, src_gid, filename, oe))
@@ -275,19 +276,20 @@ def migrate_subgroup_info(subgroup):
             full_path_with_parent_namespace))
         dst_group = groups.find_group_by_path(
             b.config.destination_host, b.config.destination_token, full_path_with_parent_namespace)
-        dst_gid = dst_group.get("id", None)
-        if dst_gid:
-            b.log.info("{0}Sub-group {1} (ID: {2}) found on destination".format(
-                get_dry_log(_DRY_RUN), full_path, dst_gid))
-            if not _DRY_RUN:
-                # Migrate CI/CD Variables
-                variables.migrate_variables(
-                    dst_gid, src_gid, "group", full_path)
-                # Remove import user
-                groups.remove_import_user(dst_gid)
-        else:
-            b.log.info("{0}Sub-group {1} NOT found on destination".format(
-                get_dry_log(_DRY_RUN), full_path_with_parent_namespace))
+        if dst_group:
+            dst_gid = dst_group.get("id", None)
+            if dst_gid:
+                b.log.info("{0}Sub-group {1} (ID: {2}) found on destination".format(
+                    get_dry_log(_DRY_RUN), full_path, dst_gid))
+                if not _DRY_RUN:
+                    # Migrate CI/CD Variables
+                    variables.migrate_variables(
+                        dst_gid, src_gid, "group", full_path)
+                    # Remove import user
+                    groups.remove_import_user(dst_gid)
+            else:
+                b.log.info("{0}Sub-group {1} NOT found on destination".format(
+                    get_dry_log(_DRY_RUN), full_path_with_parent_namespace))
     except (RequestException, KeyError, OverflowError) as oe:
         b.log.error(
             "Failed to migrate sub-group {0} (ID: {1}) info with error:\n{2}".format(full_path, src_gid, oe))
