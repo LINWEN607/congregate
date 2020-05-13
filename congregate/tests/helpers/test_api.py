0d27b74616e26cdc256c70656f7e42936a7d0136
Petar Prokic <pprokic@gitlab.com>
2020-05-13 20:56:46 +0200

    Added keyset-based pagination for listing projects; Added UTs

--- congregate/tests/helpers/test_api.py
+++ congregate/tests/helpers/test_api.py
@@ -1,5 +1,6 @@
-import congregate.helpers.api as api
 import mock
+
+import congregate.helpers.api as api
 
 
 def test_generate_v4_get_request_url():
@@ -77,7 +78,8 @@ def test_generate_post_request_calls_generate_v4_request_header_when_header_is_n
 def test_generate_post_request_does_not_call_generate_v4_request_header_when_header_is_not_none(p, r, h):
     h.return_value = "some_header"
     p.return_value = r
-    api.generate_post_request("host", "token", "api", "data", headers="some_header")
+    api.generate_post_request("host", "token", "api",
+                              "data", headers="some_header")
     h.assert_not_called()
 
 
@@ -107,7 +109,8 @@ def test_generate_put_request_calls_generate_v4_request_header_when_header_is_no
 def test_generate_put_request_does_not_call_generate_v4_request_header_when_header_is_not_none(p, r, h):
     h.return_value = "some_header"
     p.return_value = r
-    api.generate_put_request("host", "token", "api", "data", headers="some_header")
+    api.generate_put_request("host", "token", "api",
+                             "data", headers="some_header")
     h.assert_not_called()
 
 
@@ -178,6 +181,7 @@ def test_search(g, resp):
     g.assert_called()
     assert j == resp.json()
 
+
 def test_audit_message_with_message():
     message = "Modifying some data"
     url = "https://gitlab.test.io"
@@ -189,6 +193,7 @@ def test_audit_message_with_message():
     expected = "Modifying some data by generating POST request to https://gitlab.test.io with data: {'test': 'data'}"
     actual = api.generate_audit_log_message(req_type, message, url, data)
     assert expected == actual
+
 
 def test_audit_message_without_message():
     message = None
@@ -202,6 +207,7 @@ def test_audit_message_without_message():
     actual = api.generate_audit_log_message(req_type, message, url, data)
     assert expected == actual
 
+
 def test_audit_message_without_data():
     message = "Modifying some data"
     url = "https://gitlab.test.io"
@@ -211,3 +217,38 @@ def test_audit_message_without_data():
     expected = "Modifying some data by generating DELETE request to https://gitlab.test.io"
     actual = api.generate_audit_log_message(req_type, message, url, data)
     assert expected == actual
+
+
+def test_get_params_none():
+    params = {
+        "page": 5,
+        "per_page": 100
+    }
+    assert params == api.get_params(None, 100, 5, False, "")
+
+
+def test_get_params_none_keyset():
+    params = {
+        "pagination": "keyset",
+        "per_page": 100,
+        "order_by": "id",
+        "sort": "asc",
+        "id_after": 42
+    }
+    assert params == api.get_params(None, 100, 5, True, 42)
+
+
+def test_get_params():
+    params = {"test": "test"}
+    params["page"] = 5
+    params["per_page"] = 100
+    assert params == api.get_params({"test": "test"}, 100, 5, False, "")
+
+
+def test_get_last_id():
+    links = '<https://gitlab.com/api/v4/projects?id_after=26924&membership=false&order_by=id&owned=false&page=1&pagination=keyset&per_page=100&repository_checksum_failed=false&simple=false&sort=asc&starred=false&statistics=false&wiki_checksum_failed=false&with_custom_attributes=false&with_issues_enabled=false&with_merge_requests_enabled=false>; rel="next"'
+    assert api.get_last_id(links) == "26924"
+
+
+def test_get_last_id_none():
+    assert api.get_last_id(None) is None
