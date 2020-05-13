93370bfc57378a852a77a6d30ff2c112233175d9
Petar Prokic <pprokic@gitlab.com>
2020-05-13 20:08:17 +0200

    Added is_error_message_present check to listed projects

--- congregate/cli/list_projects.py
+++ congregate/cli/list_projects.py
@@ -1,6 +1,7 @@
 import json
 import os
 from congregate.helpers.base_class import BaseClass
+from congregate.helpers.misc_utils import is_error_message_present
 from congregate.migration.gitlab.groups import GroupsClient
 from congregate.migration.gitlab.users import UsersClient
 from congregate.migration.gitlab.api.projects import ProjectsApi
@@ -17,29 +18,24 @@ def list_projects():
     """
     b.log.info("Listing projects from source {}:".format(b.config.source_host))
 
-    projects = list(
-        projects_api.get_all_projects(
-            b.config.source_host,
-            b.config.source_token))
+    projects = list(projects_api.get_all_projects(
+        b.config.source_host, b.config.source_token))
 
     with open("%s/data/project_json.json" % b.app_path, "wb") as f:
         json.dump(projects, f, indent=4)
 
     for project in projects:
-        b.log.info(
-            u"[ID: {0}] {1}: {2}".format(
-                project["id"],
-                project["name"],
-                project["description"]))
+        if is_error_message_present(project):
+            b.log.error(
+                "Failed to list project with response: {}".format(project))
+        else:
+            b.log.info(u"[ID: {0}] {1}: {2}".format(
+                project["id"], project["name"], project["description"]))
 
     groups.retrieve_group_info(
-        b.config.source_host,
-        b.config.source_token,
-        quiet=True)
-    users.retrieve_user_info(
-        b.config.source_host,
-        b.config.source_token,
-        quiet=True)
+        b.config.source_host, b.config.source_token, quiet=True)
+    users.retrieve_user_info(b.config.source_host,
+                             b.config.source_token, quiet=True)
 
     staged_files = ["stage", "staged_groups", "staged_users"]
 
