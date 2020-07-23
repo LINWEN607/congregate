45dd980182759810be83d1822f842cf700678ae5
Invited User iu0pybam <nperic@gitlab.com>
2020-07-23 17:06:01 -0400

    Added verify=False

--- congregate/migration/github/api/users.py
+++ congregate/migration/github/api/users.py
@@ -1,3 +1,4 @@
+
 from congregate.migration.github.api.base import GitHubApi
 
 
@@ -7,7 +8,6 @@ class UsersApi():
         self.token = token
         self.api = GitHubApi(self.host, self.token)
     def get_all_users(self):
-        return self.api.list_all(self.host, "users")
+        return self.api.list_all(self.host, "users", verify=False)
     def get_user(self, slug):
-        return self.api.__generate_v3_request_url(self.host, "users/{}".format(slug))
-
+        return self.api.generate_v3_get_request(self.host, "users/{}".format(slug), verify=False)
