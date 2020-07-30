4026e10b2baffc28650504d1d814f021ba5b2cd2
Petar Prokic <pprokic@gitlab.com>
2020-07-30 15:57:37 +0200

    Update based on review
    - Use safe_json_response for single GET calls
    - Always return from methods for easier testing
    - Use name orgs instead of obj where applicable

--- congregate/migration/github/orgs.py
+++ congregate/migration/github/orgs.py
@@ -1,7 +1,7 @@
 import json
 
 from congregate.helpers.base_class import BaseClass
-from congregate.helpers.misc_utils import remove_dupes
+from congregate.helpers.misc_utils import remove_dupes, safe_json_response
 from congregate.migration.github.api.orgs import OrgsApi
 from congregate.migration.github.repos import ReposClient
 from congregate.migration.github.users import UsersClient
@@ -28,23 +28,23 @@ class OrgsClient(BaseClass):
         While traversing orgs gather repo, team and member metadata.
         """
         repos = self.get_formatted_repos()
-        obj = []
+        orgs = []
         for org in self.orgs_api.get_all_orgs():
-            self.add_orgs_as_groups(obj, org["login"], repos)
+            self.add_orgs_as_groups(orgs, org["login"], repos)
             for team in self.orgs_api.get_all_org_teams(org["login"]):
                 self.add_teams_as_subgroups(
-                    obj, org["login"], team, repos)
+                    orgs, org["login"], team, repos)
         with open("{}/data/groups.json".format(self.app_path), "w") as f:
-            json.dump(remove_dupes(obj), f, indent=4)
+            json.dump(remove_dupes(orgs), f, indent=4)
         with open("{}/data/project_json.json".format(self.app_path), "w") as f:
             json.dump(remove_dupes(repos), f, indent=4)
-        return remove_dupes(obj)
+        return remove_dupes(orgs)
 
-    def add_orgs_as_groups(self, obj, org_name, repos):
+    def add_orgs_as_groups(self, orgs, org_name, repos):
         org_repos = self.orgs_api.get_all_org_repos(org_name)
         # repos.extend(self.repos.format_repos(org_repos))
-        org = self.orgs_api.get_org(org_name).json()
-        obj.append({
+        org = safe_json_response(self.orgs_api.get_org(org_name))
+        orgs.append({
             "name": org["login"],
             "id": org["id"],
             "path": org["login"],
@@ -57,13 +57,13 @@ class OrgsClient(BaseClass):
             "members": [],
             "projects": []   # self.repos.format_repos(org_repos),
         })
-        return obj
+        return orgs
 
-    def add_teams_as_subgroups(self, obj, org_name, team, repos):
+    def add_teams_as_subgroups(self, orgs, org_name, team, repos):
         team_repos = self.orgs_api.get_all_org_team_repos(
             org_name, team["slug"])
         # repos.extend = self.repos.format_repos(team_repos)
-        obj.append({
+        orgs.append({
             "name": team["name"],
             "id": team["id"],
             "path": team["slug"],
@@ -77,11 +77,12 @@ class OrgsClient(BaseClass):
             "members": [],
             "projects": []   # self.repos.format_repos(team_repos),
         })
+        return orgs
 
     def get_team_full_path(self, org_name, team):
         full_path = [org_name, team["slug"]]
         while team["parent"]:
             full_path.insert(1, team["parent"]["slug"])
-            team = self.orgs_api.get_org_team(
-                org_name, team["parent"]["slug"]).json()
+            team = safe_json_response(self.orgs_api.get_org_team(
+                org_name, team["parent"]["slug"]))
         return "/".join(full_path)
