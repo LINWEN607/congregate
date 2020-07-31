b90755762a165a0d1b7f69641353d26e190a3bde
Mike Lindsay <mlindsay@gitlab.com>
2020-07-31 14:37:10 -0700

    getting my repo back to where it belongs

--- congregate/tests/migration/github/test_base.py
+++ congregate/tests/migration/github/test_base.py
@@ -56,16 +56,34 @@ def test_create_dict_from_headers():
     resp = ut.create_dict_from_headers(link_headers)
     assert resp['next'] == r
 
+# # pylint: disable=no-member
+# @responses.activate
+# @mock.patch("congregate.helpers.api.generate_get_request")
+# def test_list_all():
+#     r = None
+#     ut = GitHubApi("HOST", "TOKEN")
 
-def test_list_all():
-    pass
-    # TODO:
-    #   1. Mock the data
-    #   2. Mock the request call
-    #   3. Mock the response
-    # r = None
-    # ut = GitHubApi("HOST", "TOKEN")
-    # ut.list_all("HOST", "organizations", verify=False)
+
+
+    # # pylint: disable=no-member
+    # @responses.activate
+    # @mock.patch("congregate.helpers.api.list_all")
+    # @mock.patch("congregate.helpers.api.generate_get_request")
+    # def test_is_group_non_empty_true_subgroups(self, mock_get_api, mock_list_all):
+    #     url_value = "https://gitlab.com/api/v4/groups"
+    #     mock_list_all.return_value = self.mock_groups.get_all_subgroups_list()
+    #     responses.add(
+    #         responses.GET,
+    #         url_value,
+    #         json=self.mock_groups.get_group(),
+    #         status=200,
+    #         content_type='text/json',
+    #         match_querystring=True)
+    #     group = self.mock_groups.get_group()
+    #     group["projects"] = []
+    #     self.assertTrue(self.groups.is_group_non_empty(group))
+
+
 
 
 def test_generate_v3_get_request():
