698fe9af9245ca5ebe4ad3045e79edebd67e4e2c
Jordan Ng <jordanng@gitlab.com>
2020-08-04 16:52:14 -0700

    Initial API code checkin

---
+++ congregate/migration/jenkins/api/base.py
@@ -0,0 +1,118 @@
+import jenkins
+
+
+class JenkinsApi():
+    def __init__(self):
+        self.host = 'http://jenkins.host.url'
+        # Jenkins API token generated within Jenkins instance by going to User Profile -> Configure -> API Token
+        self.token = 'jenkins_api_token'
+        self.user = 'jenkins_username'
+
+        # Connect to server
+        self.server = jenkins.Jenkins(self.host, username=self.user, password=self.token)
+
+    def list_jobs(self):
+        '''
+        https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.get_jobs
+        Parameters:
+        folder_depth - Number of levels to search, int. By default 0, which will limit search to toplevel. None disables the limit.
+        view_name - Name of a Jenkins view for which to retrieve jobs, str. By default, the job list is not limited to a specific view.
+        Returns:	list of jobs, [{str: str, str: str, str: str, str: str}]
+        '''
+        jobs = self.server.get_jobs()
+        print(jobs)
+
+    def get_all_jobs(self):
+        # https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.get_all_jobs
+        jobs_list = self.server.run_script("""
+            import groovy.json.JsonBuilder;
+
+            // get all projects excluding matrix configuration
+            // as they are simply part of a matrix project.
+            // there may be better ways to get just jobs
+            items = Jenkins.instance.getAllItems(AbstractProject);
+            items.removeAll {
+                it instanceof hudson.matrix.MatrixConfiguration
+            };
+
+            def json = new JsonBuilder()
+            def root = json {
+                jobs items.collect {
+                    [
+                    name: it.name,
+                    url: Jenkins.instance.getRootUrl() + it.getUrl(),
+                    color: it.getIconColor().toString(),
+                    fullname: it.getFullName()
+                    ]
+                }
+            }
+
+            // use json.toPrettyString() if viewing
+            println json.toString()
+        """)
+
+        print(jobs_list)
+
+    def list_all_scm(self):
+        # Get all SCM Git urls listed, uses https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.run_script
+        scmallinfo = self.server.run_script("""
+            Jenkins.instance.getAllItems(hudson.model.AbstractProject.class).each
+            {
+                it ->
+                scm = it.getScm()
+                if(scm instanceof hudson.plugins.git.GitSCM)
+                {
+                    println scm.getUserRemoteConfigs()[0].getUrl()
+                }
+            }
+        """)
+
+        print(scmallinfo)
+
+    def get_job_config(self, job_name):
+        '''
+        https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.get_job_config
+        Parameters:	name - Name of Jenkins job, str
+        Returns:    job configuration (XML format)
+        '''
+        my_job = self.server.get_job_config(job_name)
+        print(my_job)
+
+    def get_job_info(self, job_name):
+        '''
+        https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.get_job_info
+        name - Job name, str
+        depth - JSON depth, int
+        fetch_all_builds - If true, all builds will be retrieved from Jenkins. Otherwise, Jenkins will only return the most recent 100 builds. 
+        This comes at the expense of an additional API call which may return significant amounts of data. bool
+        Returns:    dictionary of job information
+        '''
+        my_job = self.server.get_job_info(job_name, 4)
+        print(my_job)
+
+    def get_info(self):
+        '''
+        https://python-jenkins.readthedocs.io/en/latest/api.html#jenkins.Jenkins.get_info
+        Parameters:
+        item - item to get information about on this Master
+        query - xpath to extract information about on this Master
+        Returns:	dictionary of information about Master or item, dict
+        '''
+        data = self.server.get_info()
+        print(data)
+
+    def get_scm_by_job(self, job_name):
+        scminfo = self.server.run_script(f"""
+        item = Jenkins.instance.getItemByFullName("{ job_name }")
+        println item.getScm().getUserRemoteConfigs()[0].getUrl()
+        """)
+        print(scminfo)
+
+
+def main():
+    # Call functions with JenkinsApi().list_jobs()
+    pass
+
+
+if __name__ == "__main__":
+    main()
