83b12a4a310ddf31c48f53bd6cafbbffe8048c54
Petar Prokic <pprokic@gitlab.com>
2020-08-12 15:37:23 +0200

    Update based on review
    - Run pytest html coverage and add results as artifact
    - Add get_results UT for error message present

--- congregate/tests/helpers/test_migrate_utils.py
+++ congregate/tests/helpers/test_migrate_utils.py
@@ -574,6 +574,15 @@ class MigrateTests(unittest.TestCase):
         self.assertEqual(mutils.get_results(results), {
                          "Total": 3, "Successful": 0})
 
+    def test_get_results_error_message(self):
+        results = [
+            {"import1": {"key": "value"}},
+            {"import2": {"message": "Failed to import"}},
+            {"import3": {"key": 1}}
+        ]
+        self.assertEqual(mutils.get_results(results), {
+                         "Total": 3, "Successful": 2})
+
     def test_is_top_level_group(self):
         self.assertTrue(mutils.is_top_level_group(
             self.mock_groups.get_group()))
