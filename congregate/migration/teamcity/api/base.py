b560af64f95a41fb6097d45b2c3fb6287a5a6db6
Jordan NG <jordanng@gitlab.com>
2020-09-02 22:01:54 +0000

    Initial checkin for TC API calls

---
+++ congregate/migration/teamcity/api/base.py
@@ -0,0 +1,79 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from congregate.helpers.misc_utils import xml_to_dict
+from congregate.helpers.decorators import stable_retry
+from congregate.helpers.logger import myLogger
+
+
+class TeamcityApi():
+    def __init__(self, host, user, token):
+        self.log = myLogger(__name__)
+        self.host = host
+        self.token = token
+        self.user = user
+
+    def generate_tc_request_url(self, host, api):
+        return "%s/app/rest/%s" % (host, api)
+
+    def generate_request_headers(self):
+        return {
+            'Content-Type': 'application/json'
+        }
+
+    def get_authorization(self):
+        return HTTPBasicAuth(self.user, self.token)
+
+    @stable_retry
+    def generate_get_request(self, api, url=None, params=None):
+        """
+        Generates GET request to TeamCity API.
+        You will need to provide the TC host, user, access token, and specific api url.
+
+            :param host: (str) TeamCity host URL
+            :param api: (str) Specific TeamCity API endpoint (ex: buildTypes)
+            :param url: (str) A URL to a location not part of the TeamCity API. Defaults to None
+            :param params:
+            :return: The response object *not* the json() or text()
+
+        """
+
+        if url is None:
+            url = self.generate_tc_request_url(self.host, api)
+
+        headers = self.generate_request_headers()
+
+        if params is None:
+            params = {}
+
+        auth = self.get_authorization()
+        return requests.get(url, params=params, headers=headers, auth=auth)
+
+    def list_build_configs(self):
+        """
+        Returns a dictionary of list of all build configurations on the TeamCity server.
+        """
+        return xml_to_dict(self.generate_get_request("buildTypes").text)
+
+    def get_build_config(self, jobid):
+        """
+        Returns a dictionary of a specific build configuration on the TeamCity server.
+        """
+        return xml_to_dict(self.generate_get_request("buildTypes/%s" % jobid).text)
+
+    def get_build_params(self, jobid):
+        """
+        Returns a dictionary of parameters to a specific build configuration on the TeamCity server.
+        """
+        return xml_to_dict(self.generate_get_request("buildTypes/%s/parameters" % jobid).text)
+
+    def get_build_vcs_roots(self, jobid):
+        """
+        Returns a dictionary of vcs entries to a specific build configuration on the TeamCity server.
+        """
+        return xml_to_dict(self.generate_get_request("buildTypes/%s/vcs-root-entries" % jobid).text)
+
+    def list_vcs_configs(self):
+        """
+        Returns a dictionary of list of all vcs roots on the TeamCity server.
+        """
+        return xml_to_dict(self.generate_get_request("vcs-roots").text)
