e7a9d75b829eb826c04795f05b45c751b202dd55
Jordan Ng <jordanng@gitlab.com>
2020-09-08 11:52:06 -0700

    Fixed UT

--- congregate/tests/migration/teamcity/test_base.py
+++ congregate/tests/migration/teamcity/test_base.py
@@ -1,6 +1,7 @@
 import unittest
 from mock import patch, PropertyMock
 from congregate.tests.mockapi.teamcity.parameters import ParametersApi
+from congregate.tests.mockapi.teamcity.buildconfigs import TeamcityJobsApi
 from congregate.migration.teamcity.base import TeamcityClient
 
 
@@ -23,7 +24,7 @@ class BaseTests(unittest.TestCase):
             'key': 'Checkbox_Param',
             'masked': False,
             'protected': False,
-            'value': "True",
+            'value': "true",
             'variable_type': 'env_var'}
         actual = client.transform_ci_variables(test_results)
         self.assertDictEqual(expected, actual)
@@ -49,4 +50,20 @@ class BaseTests(unittest.TestCase):
             'value': "No Default Value",
             'variable_type': 'env_var'}
         actual = client.transform_ci_variables(test_results)
+        self.assertDictEqual(expected, actual)
+
+    @patch('congregate.helpers.conf.Config.ci_source_type', new_callable=PropertyMock)
+    @patch('congregate.helpers.conf.Config.ci_source_host', new_callable=PropertyMock)
+    @patch('congregate.helpers.conf.Config.ci_source_username', new_callable=PropertyMock)
+    @patch('congregate.helpers.conf.Config.ci_source_token', new_callable=PropertyMock)
+    def test_retrieve_jobs_with_vcs_info(self, token, username, host, source_type):
+        token.return_value = 'abc123'
+        username.return_value = 'abc123'
+        host.return_value = 'http://example.teamcity.com/'
+        source_type.return_value = 'abc123'
+        params = TeamcityJobsApi()
+        expected = params.get_job_config_dict()
+        client = TeamcityClient()
+
+        actual = client.retrieve_jobs_with_vcs_info()
         self.assertDictEqual(expected, actual)
