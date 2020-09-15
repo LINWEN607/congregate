40e501448a6ff799767f09246ee453990f974774
Michael Leopard <mleopard@gitlab.com>
2020-09-15 13:44:32 +0000

    fixing runbook syntax issue

--- runbookscript.py
+++ runbookscript.py
@@ -11,20 +11,26 @@ conn = http.client.HTTPSConnection("gitlab.com")
 arr = [x for x in os.listdir("runbooks") if x != "README.md"]
 arr_length = len(arr)
 for i, x in enumerate(arr):
-    xpath = os.path.join(path,x)
+    xpath = os.path.join(path, x)
     with open(xpath, 'r') as f:
         l = f.read()
+        data = {
+            "branch": "master", 
+            "author_email": "sfirdaus@gitlab.com",
+            "author_name": "Syeda Firdaus", 
+            "content": str(l)
+        }
         if i != (arr_length - 1):
-            data = {"branch": "master", "author_email": "sfirdaus@gitlab.com", "author_name": "Syeda Firdaus", "commit_message": "Uploading runbook files [skip-ci]", "content": str(l)}
+            data["commit_message"] = "Uploading runbook files [skip-ci]"
         else:
-            data = {"branch": "master", "author_email": "sfirdaus@gitlab.com", "author_name": "Syeda Firdaus", "commit_message": "Uploading runbook files", "content": str(l)}
-            payload = json.dumps(data)
-            headers = {
-                'private-token': os.getenv("PS_MIGRATION_TOKEN"),
-                'content-type': "application/json"
-            }
-            url = f"/api/v4/projects/{project_id}/repository/files/.gitlab%2Fissue_templates%2F"+x+""
-            conn.request("POST", url, payload, headers)
+            data["commit_message"] = "Uploading runbook files"
+        payload = json.dumps(data)
+        headers = {
+            'private-token': os.getenv("PS_MIGRATION_TOKEN"),
+            'content-type': "application/json"
+        }
+        url = f"/api/v4/projects/{project_id}/repository/files/.gitlab%2Fissue_templates%2F"+x+""
+        conn.request("POST", url, payload, headers)
 
         res = conn.getresponse()
         data = res.read()
