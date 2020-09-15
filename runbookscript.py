55b2a91d9dd7c8afb079fb4d682cc9691fb16d6e
Michael Leopard <mleopard@gitlab.com>
2020-09-15 12:36:11 +0000

    Update runbook script to work more with migration template

--- runbookscript.py
+++ runbookscript.py
@@ -7,13 +7,16 @@ project_id = sys.argv[-1]
 
 files = os.listdir('runbooks')
 path = 'runbooks/'
-arr = os.listdir(path)
 conn = http.client.HTTPSConnection("gitlab.com")
-for x in arr:
-    if x != "README.md":
-        xpath = os.path.join(path,x)
-        with open(xpath, 'r') as f:
-            l = f.read()
+arr = [x for x in os.listdir("runbooks") if x != "README.md"]
+arr_length = len(arr)
+for i, x in enumerate(arr):
+    xpath = os.path.join(path,x)
+    with open(xpath, 'r') as f:
+        l = f.read()
+        if i != (arr_length - 1):
+            data = {"branch": "master", "author_email": "sfirdaus@gitlab.com", "author_name": "Syeda Firdaus", "commit_message": "Uploading runbook files [skip-ci]", "content": str(l)}
+        else:
             data = {"branch": "master", "author_email": "sfirdaus@gitlab.com", "author_name": "Syeda Firdaus", "commit_message": "Uploading runbook files", "content": str(l)}
             payload = json.dumps(data)
             headers = {
@@ -23,13 +26,13 @@ for x in arr:
             url = f"/api/v4/projects/{project_id}/repository/files/.gitlab%2Fissue_templates%2F"+x+""
             conn.request("POST", url, payload, headers)
 
-            res = conn.getresponse()
-            data = res.read()
-            print(res.status)
-            print(data.decode("utf-8"))
-            if res.status == 400:
-                conn.request("PUT", url, payload, headers)
-                res1 = conn.getresponse()
-                data1 = res1.read()
-                print(res1.status)
-                print(data1.decode("utf-8"))
+        res = conn.getresponse()
+        data = res.read()
+        print(res.status)
+        print(data.decode("utf-8"))
+        if res.status == 400:
+            conn.request("PUT", url, payload, headers)
+            res1 = conn.getresponse()
+            data1 = res1.read()
+            print(res1.status)
+            print(data1.decode("utf-8"))
