bd52d1935b91c7daf3d6c78306d64584f3b729e3
Michael Leopard <mleopard@gitlab.com>
2020-11-02 18:39:53 +0000

    update GH user tests

--- congregate/tests/migration/github/test_users.py
+++ congregate/tests/migration/github/test_users.py
@@ -12,7 +12,11 @@ from congregate.helpers.mdbc import MongoConnector
 from congregate.tests.mockapi.github.users import MockUsersApi
 from congregate.migration.github.users import UsersClient
 from congregate.migration.github.api.users import UsersApi
+from congregate.migration.github.meta.github_browser import GitHubBrowser
 
+class mock_github_browser():
+    def scrape_user_email(self, x):
+        return None
 
 @pytest.mark.unit_test
 class UsersTests(unittest.TestCase):
@@ -26,6 +30,8 @@ class UsersTests(unittest.TestCase):
         self.mongo_mock.drop_collection("groups")
         self.mongo_mock.drop_collection("users")
 
+    @patch.object(UsersClient, "establish_browser_connection")
+    @patch.object(GitHubBrowser, "scrape_user_email")
     @patch("io.TextIOBase")
     @patch('builtins.open')
     @patch.object(UsersApi, "get_all_users")
@@ -40,7 +46,11 @@ class UsersTests(unittest.TestCase):
                                 mock_single_user,
                                 mock_users,
                                 mock_open,
-                                mock_file):
+                                mock_file,
+                                mock_scrape_user_email,
+                                browser_init):
+        browser_init.side_effect = [mock_github_browser(), mock_github_browser(), mock_github_browser()]
+        mock_scrape_user_email.side_effect = ['test@email.com', 'test@email.com', 'test@email.com']
         mock_source_token.return_value = "token"
         mock_source_host.return_value = "https://github.com"
         mock_user1 = MagicMock()
@@ -53,6 +63,7 @@ class UsersTests(unittest.TestCase):
         type(mock_user3).status_code = PropertyMock(return_value=200)
         mock_user3.json.return_value = self.mock_users.get_user()[2]
         mock_single_user.side_effect = [mock_user1, mock_user2, mock_user3]
+
 
         mock_users.return_value = self.mock_users.get_all_users()
         mock_open.return_value = mock_file
@@ -76,15 +87,6 @@ class UsersTests(unittest.TestCase):
                 "email": None
             },
             {
-                "username": "github-enterprise",
-                "name": None,
-                "id": 2,
-                "state": "active",
-                "avatar_url": "https://github.gitlab-proserv.net/avatars/u/2?",
-                "is_admin": False,
-                "email": None
-            },
-            {
                 "username": "gitlab",
                 "name": None,
                 "id": 3,
@@ -97,14 +99,23 @@ class UsersTests(unittest.TestCase):
 
         self.assertGreater(len(actual_users), 0)
 
+        print(actual_users)
+        print(expected_users)
+
         for i, _ in enumerate(expected_users):
             self.assertDictEqual(expected_users[i], actual_users[i])
 
+    @patch.object(UsersClient, "establish_browser_connection")
+    @patch.object(GitHubBrowser, "scrape_user_email")
     @patch.object(UsersApi, "get_user")
     @patch("congregate.helpers.conf.Config.source_host", new_callable=PropertyMock)
     def test_format_users_with_permissions(self,
                                            mock_source_host,
-                                           mock_single_user):
+                                           mock_single_user,
+                                           mock_scrape_user_email,
+                                           browser_init):
+        browser_init.side_effect = [mock_github_browser(), mock_github_browser(), mock_github_browser()]
+        mock_scrape_user_email.side_effect = ['test@email.com', 'test@email.com', 'test@email.com']
         mock_source_host.return_value = "https://github.com"
 
         mock_user1 = MagicMock()
@@ -160,11 +171,17 @@ class UsersTests(unittest.TestCase):
         self.assertEqual(actual_users.sort(
             key=lambda x: x["id"]), expected_users.sort(key=lambda x: x["id"]))
 
+    @patch.object(UsersClient, "establish_browser_connection")
+    @patch.object(GitHubBrowser, "scrape_user_email")
     @patch.object(UsersApi, "get_user")
     @patch("congregate.helpers.conf.Config.source_host", new_callable=PropertyMock)
     def test_format_users_with_error(self,
                                      mock_source_host,
-                                     mock_single_user):
+                                     mock_single_user,
+                                     mock_scrape_user_email,
+                                     browser_init):
+        browser_init.side_effect = [mock_github_browser(), mock_github_browser(), mock_github_browser()]
+        mock_scrape_user_email.side_effect = ['test@email.com', 'test@email.com', 'test@email.com']
         mock_source_host.return_value = "https://github.com"
 
         mock_user1 = MagicMock()
