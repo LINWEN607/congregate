efb6cd2f82440c62aba03441c453a227123ee9dc
Michael Leopard <mleopard@gitlab.com>
2021-09-15 13:21:47 +0000

    Updating loggers to not require a file path location

--- congregate/helpers/audit_logger.py
+++ congregate/helpers/audit_logger.py
@@ -1,11 +1,12 @@
 import logging
+from os.path import exists
 
 loggers = {}
 log_file_format = \
     "[%(asctime)s][%(levelname)s]| %(message)s"
 
 
-def audit_logger(name, app_path='.'):
+def audit_logger(name, app_path='.', log_dir='data/logs'):
     name = name + "_audit"
     global loggers
     if loggers.get(name):
@@ -13,15 +14,16 @@ def audit_logger(name, app_path='.'):
     else:
         logger = logging.getLogger(name)
         logger.setLevel(logging.INFO)
-        file_log_handler = logging.FileHandler(
-            f'{app_path}/data/logs/audit.log')
         stderr_log_handler = logging.StreamHandler()
-
         formatter = logging.Formatter(
             log_file_format, datefmt="%d %b %Y %H:%M:%S")
-        file_log_handler.setFormatter(formatter)
+        log_dir = f'{app_path}/{log_dir}'
+        if exists(log_dir):
+            log_path = f'{log_dir}/audit.log'
+            file_log_handler = logging.FileHandler(log_path)
+            file_log_handler.setFormatter(formatter)
+            logger.addHandler(file_log_handler)
         stderr_log_handler.setFormatter(formatter)
-        logger.addHandler(file_log_handler)
         logger.addHandler(stderr_log_handler)
         loggers[name] = logger
 
