073f564180b7e4d0d5a0098754837d080a34f3b8
Michael Leopard <mleopard@gitlab.com>
2021-10-19 19:28:48 +0000

    mocking staged_projects

--- congregate/tests/migration/gitlab/test_projects.py
+++ congregate/tests/migration/gitlab/test_projects.py
@@ -440,10 +440,12 @@ class ProjectsTests(unittest.TestCase):
         mock_staged.return_value = self.mock_projects.get_staged_forked_projects()
         self.assertIsNone(self.projects.create_staged_projects_fork_relation())
 
+    @patch("congregate.helpers.migrate_utils.read_json_file_into_object")
     @patch("congregate.helpers.migrate_utils.get_dst_path_with_namespace")
     @patch.object(ProjectsClient, "find_project_by_path")
-    def test_create_staged_projects_fork_relation_exception(self, mock_find_id, mock_get_path):
+    def test_create_staged_projects_fork_relation_exception(self, mock_find_id, mock_get_path, mock_staged):
         mock_get_path.return_value = "top-level-group/security-reports-fork"
         mock_find_id.side_effect = RequestException()
+        mock_staged.return_value = self.mock_projects.get_staged_forked_projects()
         with self.assertLogs(self.projects.log, level="ERROR"):
             self.projects.create_staged_projects_fork_relation()
