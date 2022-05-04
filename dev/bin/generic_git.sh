4c0fc909fd6d3d42a621514459c1d29cf5cade98
Michael Leopard <mleopard@gitlab.com>
2022-05-04 11:43:18 +0000

    generic git code review changes

--- dev/bin/generic_git.sh
+++ dev/bin/generic_git.sh
@@ -1,42 +1,40 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-repo_name="$1"
-target_group="$2"
+CLONE_URL="$1"
 
-push_branches='refs/remotes/old-origin/*:refs/heads/*'
+REPO_NAME="${CLONE_URL##*/}"
+
+PUSH_BRANCHES='refs/remotes/old-origin/*:refs/heads/*'
 
 function usage(){  
-    echo "Usage: $0 [original repo with group name] [target group_name] "  
-    echo "Example: $0 abc.git <test> "
-    echo "During the script, user need to input source SSH clone url such as ssh://<username>@<the server name>:29418"
-    echo "And destination SSH clone url such as git@gitlab.com"
+    echo "Usage: $0 [original repo with group name]"  
+    echo "Example: $0 git@gitlab.com:namespace/abc.git"
+    echo "During the script, user needs to input source SSH/HTTPS clone url such as ssh://<username>@<the server name>:29418"
+    echo "And destination SSH/HTTPS clone url such as git@gitlab.com"
     exit 1  
- } 
+} 
 
-if [ $# -ne 2 ] ; then
+if [ $# -ne 1 ] ; then
     usage
 else
-    echo "What is the clone URL from source instance?"
-    echo "For example, for Gerrit migration:  ssh://<username>@<the server name>:29418"
-    read source_clone_url
-    echo "The source repository clone URL: $source_clone_url/${repo_name##*/}"
-    git clone "$source_clone_url/$repo_name" 
-    cd ${repo_name##*/}
-    echo "where am I"
-    pwd
-    echo "What is the clone URL for destination instance?"
-    echo "For example, for GitLab: git@gitlab.com"
-    read dest_clone_url
-    echo "The new repo url: $dest_clone_url:$target_group/${repo_name}"
+    echo "The source repository clone URL: $CLONE_URL"
+    git clone "$CLONE_URL"
+    cd "${REPO_NAME/\.git/}"
+    echo "In $(pwd)"
+    echo "What is the path for the destination instance?"
+    echo "For example, for GitLab: git@gitlab.com:namespace for SSH and https://gitlab.example.com/namespace for HTTPS"
+    echo "Include the clone URL up to the Git repo, but not the repo itself"
+    read DEST_CLONE_URL
+    echo "The new repo url: $DEST_CLONE_URL/${REPO_NAME}"
     read -p 'Press [Enter] key to continue...'
     git remote rename origin old-origin 
-    git remote add origin "git@gitlab.com:$target_group/${repo_name}"
-    echo "Pushing all the branches from Gerrit to GitLab including LFS"
-    git push origin $push_branches
+    git remote add origin "$DEST_CLONE_URL/${REPO_NAME}"
+    echo "Pushing all the branches from generic Git repository to GitLab including LFS"
+    git push origin $PUSH_BRANCHES || return 1
     read -p 'Press [Enter] key to continue...'
-    echo "Pushing all the Tags from Gerrit to GitLab including LFS"
-    git push -u origin --tags
-    echo "Migration Completes!"
+    echo "Pushing all the Tags from generic Git repository to GitLab including LFS"
+    git push -u origin --tags || return 1
+    echo "Migration Complete!"
 fi
 
 
