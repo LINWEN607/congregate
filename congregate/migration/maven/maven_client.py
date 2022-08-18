69ed1a1321960e38065681dd4f886aac8753b688
Michael Leopard <mleopard@gitlab.com>
2022-08-18 19:57:20 +0000

    Fixing message type in maven client

--- congregate/migration/maven/maven_client.py
+++ congregate/migration/maven/maven_client.py
@@ -7,15 +7,19 @@ from congregate.migration.maven import maven_pb2_grpc
 def get_package(**kwargs):
     """
         Calls RPC service to download maven package to maven container
+
+        Refer to /protos/maven.proto for available parameters
     """
     with grpc.insecure_channel(f"{os.getenv('HOST_IP')}:50051") as channel:
         stub = maven_pb2_grpc.MavenCommandHandlerStub(channel)
-        response = stub.GetPackage(maven_pb2.CommandArgs(**kwargs))
+        response = stub.GetPackage(maven_pb2.GetPackageArgs(**kwargs))
     return response.output, response.exitCode
 
 def deploy_package(**kwargs):
     """
         Calls RPC service to deploy maven package to GitLab repo
+
+        Refer to /protos/maven.proto for available parameters
     """
     with grpc.insecure_channel(f"{os.getenv('HOST_IP')}:50051") as channel:
         stub = maven_pb2_grpc.MavenCommandHandlerStub(channel)
