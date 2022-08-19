1bd3f453bde7ff696a22cf0fb8d2155392dd6c12
arosencrans <arosencrans@gitlab.com>
2022-08-19 12:17:49 -0400

    updated congregate/migration/gitlab/packages.py

--- congregate/migration/gitlab/packages.py
+++ congregate/migration/gitlab/packages.py
@@ -5,30 +5,56 @@ from congregate.helpers.base_class import BaseClass
 from gitlab_ps_utils.misc_utils import is_error_message_present
 from gitlab_ps_utils.dict_utils import pop_multiple_keys
 from congregate.migration.gitlab.api.packages import PackagesApi
-
+from congregate.migration.maven.maven_client import get_package, deploy_package
 
 class PackagesClient(BaseClass):
     def __init__(self):
         self.packages = PackagesApi()
         super(PackagesClient, self).__init__()
 
-    def migrate_project_packages(self, src_id, dest_id):
+    def migrate_project_packages(self, src_id, dest_id, project_name):
         self.log.info(f"Migrating project {src_id} packages")
         for package in self.packages.get_project_packages(self.config.source_host, self.config.source_token, src_id):
             if self.get_package_type(package) == 'maven':
                 executable = None
                 pom_file = None
+                packaging = None
                 for package_file in self.packages.get_package_files(self.config.source_host, self.config.source_token, src_id, package.get('id')):
                     if Path(package_file.get('file_name')).suffix == '.pom':
                         pom_file = package_file['file_name']
                     if Path(package_file.get('file_name')).suffix in ['.jar', '.war', '.ear']:
+                        packaging = (Path(package_file.get('file_name')).suffix).strip('.').upper()
                         executable = package_file['file_name']
                 if executable and pom_file:
-                    pass
+                    get_package_result = get_package(
+                        projectName=project_name,
+                        artifact=self.format_artifact(package['name'], package['version']),
+                        remoteRepositories=f"gitlab-src::::{self.config.source_host}/api/v4/projects/{src_id}/packages/maven"
+                    )
+                    if get_package_result[0] == 0 :
+                        deploy_package_result = deploy_package(
+                                projectName=project_name,
+                                groupId=self.format_groupid(package['name']),
+                                artifactId=self.format_artifactid(package['name']),
+                                version=package['version'],
+                                packaging=packaging,
+                                file=f"/opt/project_repositories/{project_name}/{package['name']}/{package['version']}/{executable}",
+                                pomFile=f"/opt/project_repositories/{project_name}/{package['name']}/{package['version']}/{pom_file}",
+                                repositoryId="gitlab-dest",
+                                url=f"{self.config.destination_host}/api/v4/projects/{dest_id}/packages/maven"
+                        )
 
             else:
                 self.log.info(f"{package.get('name')} is not a maven package and thus not supported at this time, skipping")
 
+    def format_groupid(self, name):
+        return '.'.join(name.split('/')[:-1])
+
+    def format_artifactid(self, name):
+        return name.split('/')[-1]
+
+    def format_artifact(self, name, version ):
+        return f"{name.replace('/','.')}:{version}"
 
     def get_package_type(self, package):
         return package.get("package_type")
