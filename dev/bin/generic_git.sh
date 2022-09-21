74ff90ca1b38633b536aceef6f7fd77ad84459ba
Nicki Peric <nperic@gitlab.com>
2022-09-21 12:11:10 -0400

    Use -z '' instead of '0 -ne 1' to check

--- dev/bin/generic_git.sh
+++ dev/bin/generic_git.sh
@@ -14,24 +14,28 @@ function usage(){
     exit 1  
 } 
 
-if [ $# -ne 1 ] ; then
+if [ -z "$CLONE_URL" ] ; then
     usage
 else
     echo "The source repository clone URL: $CLONE_URL"
     git clone "$CLONE_URL"
     cd "${REPO_NAME/\.git/}"
     echo "In $(pwd)"
+    
     echo "What is the path for the destination instance?"
     echo "For example, for GitLab: git@gitlab.com:namespace for SSH and https://gitlab.example.com/namespace for HTTPS"
     echo "Include the clone URL up to the Git repo, but not the repo itself"
     read DEST_CLONE_URL
+    
     echo "The new repo url: $DEST_CLONE_URL/${REPO_NAME}"
     read -p 'Press [Enter] key to continue...'
     git remote rename origin old-origin 
     git remote add origin "$DEST_CLONE_URL/${REPO_NAME}"
+    
     echo "Pushing all the branches from generic Git repository to GitLab including LFS"
     git push origin $PUSH_BRANCHES || return 1
     read -p 'Press [Enter] key to continue...'
+    
     echo "Pushing all the Tags from generic Git repository to GitLab including LFS"
     git push -u origin --tags || return 1
     echo "Migration Complete!"
