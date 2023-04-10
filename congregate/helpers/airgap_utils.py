f8b764ca67361e93ecce09d4e645322b4d941066
Michael Leopard <mleopard@gitlab.com>
2023-04-10 09:58:13 -0400

    Adding mtime to airgap artifact
    Fixing artifact filename to include single .tar.gz

--- congregate/helpers/airgap_utils.py
+++ congregate/helpers/airgap_utils.py
@@ -1,5 +1,6 @@
 import tarfile
 import os
+from time import time
 from json import dumps
 from io import BytesIO
 from gzip import GzipFile
@@ -9,11 +10,13 @@ from congregate.helpers.mdbc import MongoConnector
 from congregate.migration.meta.api_models.single_project_features import SingleProjectFeatures
     
 def create_archive(pid, export_file):
-    with tarfile.TarFile.open(f"{export_file}_artifact.tar", 'w:gz') as tar:
-        add_file(tar, Path(export_file).name, GzipFile(export_file, 'rb'))
-        add_file(tar, "project_features.json", get_project_features(pid))
+    final_path = f"{export_file.split('.tar.gz')[0]}_artifact.tar.gz"
+    with tarfile.TarFile.open(final_path, 'w:gz') as tar:
+        __add_file(tar, Path(export_file).name, GzipFile(export_file, 'rb'))
+        __add_file(tar, "project_features.json", __get_project_features(pid))
+    return final_path
 
-def get_project_features(pid):
+def __get_project_features(pid):
     mongo = MongoConnector()
     features = mongo.safe_find_one('project_features', query={"id": pid})
     return BytesIO(
@@ -22,10 +25,10 @@ def get_project_features(pid):
         ), encoding='utf-8')
     )
 
-def add_file(tar, path, data):
+def __add_file(tar, path, data):
     new_file = tarfile.TarInfo(path)
     data.seek(0, os.SEEK_END)
     new_file.size = data.tell()
     data.seek(0)
+    new_file.mtime = time()
     return tar.addfile(tarinfo=new_file, fileobj=data)
-
