b0caac5fcc339949b01833258b8a1781bd132cff
Michael Leopard <mleopard@gitlab.com>
2023-07-06 14:59:54 -0400

    Updating version check script

--- dev/bin/version_check.sh
+++ dev/bin/version_check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-STORED_VERSION=$(cat pyproject.toml | grep "version" | awk '{print $3}' | tr -d '"')
+STORED_VERSION=$(cat pyproject.toml | grep -e "^version" | awk '{print $3}' | tr -d '"')
 
 if [ "$STORED_VERSION" != "$CI_COMMIT_TAG" ]; then 
     echo "Version mismatch. Please update pyproject.toml before cutting a tag"
