80b7e6addcd84afef2acbff52ef65cd293ff0b3a
Jordan NG <jordanng@gitlab.com>
2023-07-26 17:47:10 -0700

    Update migrate function for bb

--- congregate/migration/bitbucket/keys.py
+++ congregate/migration/bitbucket/keys.py
@@ -1,20 +1,25 @@
 from gitlab_ps_utils.misc_utils import strip_netloc, is_error_message_present
+from gitlab_ps_utils.dict_utils import pop_multiple_keys
+
+from requests.exceptions import RequestException
+import json
 
 from congregate.migration.bitbucket.api.users import UsersApi
 from congregate.migration.bitbucket.base import BitBucketServer
 from congregate.helpers.mdbc import MongoConnector
+from congregate.migration.gitlab.api.users import UsersApi as GLUsersAPI
 
 
 class Client(BitBucketServer):
     def __init__(self):
         self.users_api = UsersApi()
+        self.glusers_api = GLUsersAPI()
         super().__init__()
 
     def migrate_bb_user_ssh_keys(self, old_user, new_user):
         try:
             email = old_user.get("email", None)
-            resp = self.users_api.get_all_user_ssh_keys(old_user.get(
-                "id", None), self.config.source_host, self.config.source_token)
+            resp = self.users_api.get_user_ssh_keys(old_user.get("id"))
             ssh_keys = iter(resp.json())
             self.log.info(f"Migrating user {email} SSH keys")
             for k in ssh_keys:
@@ -23,10 +28,13 @@ class Client(BitBucketServer):
                     self.log.error(
                         f"Failed to fetch user {email} SSH key ({k})")
                     return False
-                # Remove unused key-values before posting key
-                k = pop_multiple_keys(k, ["id", "created_at"])
-                self.users_api.create_user_ssh_key(
-                    self.config.destination_host, self.config.destination_token, new_user.get("id", None), k)
+                # Extract relevant data
+                data = json.loads(k)
+                values_list = data["values"]
+                extracted_data = {"text": values_list[0]["text"], "label": values_list[0]["label"]}
+
+                self.glusers_api.create_user_ssh_key(
+                    self.config.destination_host, self.config.destination_token, new_user.get("id", None), extracted_data)
             return True
         except TypeError as te:
             self.log.error(f"User {email} SSH keys {resp} {te}")
