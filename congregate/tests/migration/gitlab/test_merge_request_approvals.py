b58d18eba06a48a2979a5e4edac7fb4921202792
Petar Prokic <pprokic@gitlab.com>
2023-08-07 17:11:03 +0200

    Fix UTs

--- congregate/tests/migration/gitlab/test_merge_request_approvals.py
+++ congregate/tests/migration/gitlab/test_merge_request_approvals.py
@@ -3,24 +3,22 @@ from pytest import mark
 import congregate.migration.gitlab.merge_request_approvals as mra
 
 
-# TODO: Mock logging capture
 @mark.unit_test
 class MergeRequestApprovalsTests(unittest.TestCase):
 
-    @staticmethod
-    def test_user_search_check_and_log_new_user_empty_returns_base_approver_ids():
+    def test_user_search_check_and_log_new_user_empty_returns_base_approver_ids(self):
         m = mra.MergeRequestApprovalsClient()
-        assert m.user_search_check_and_log(
-            [], {'email': "a@abc.com"}, [1, 2, 3], "email") == [1, 2, 3]
+        with self.assertLogs(m.log, level="WARNING"):
+            self.assertEqual(m.user_search_check_and_log(
+                {}, {'email': "a@abc.com"}, [1, 2, 3], "email"), [1, 2, 3])
 
-    @staticmethod
-    def test_user_search_check_and_log_no_user_id_returns_base_approver_ids():
+    def test_user_search_check_and_log_no_user_id_returns_base_approver_ids(self):
         m = mra.MergeRequestApprovalsClient()
-        assert m.user_search_check_and_log(
-            [{"noid": 1}], {'email': "a@abc.com"}, [1, 2, 3], "email") == [1, 2, 3]
+        with self.assertLogs(m.log, level="WARNING"):
+            self.assertEqual(m.user_search_check_and_log(
+                {"noid": 1}, {'email': "a@abc.com"}, [1, 2, 3], "email"), [1, 2, 3])
 
-    @staticmethod
-    def test_user_search_check_and_log_happy():
+    def test_user_search_check_and_log_happy(self):
         m = mra.MergeRequestApprovalsClient()
-        assert m.user_search_check_and_log(
-            [{"id": 4}], {'email': "a@abc.com"}, [1, 2, 3], "email") == [1, 2, 3, 4]
+        self.assertEqual(m.user_search_check_and_log(
+            {"id": 4}, {'email': "a@abc.com"}, [1, 2, 3], "email"), [1, 2, 3, 4])
