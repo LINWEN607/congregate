85b0ef36fe04c764a8c5837033d1b8897ce11a06
Michael Leopard <mleopard@gitlab.com>
2023-11-13 14:02:53 -0500

    Updating ps-utils dep and calling strip_none from ps-utils

--- congregate/migration/meta/api_models/bulk_import.py
+++ congregate/migration/meta/api_models/bulk_import.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, asdict
 from typing import List
+from gitlab_ps_utils.dict_utils import strip_none
 from congregate.migration.meta.api_models.bulk_import_configuration import BulkImportconfiguration
 from congregate.migration.meta.api_models.bulk_import_entity import BulkImportEntity
 
@@ -10,20 +11,3 @@ class BulkImportPayload:
 
     def to_dict(self):
         return strip_none(asdict(self))
-    
-def strip_none(source, dest=None):
-    if not dest:
-        dest = {}
-    if isinstance(source, dict):
-        for k, v in source.items():
-            if v:
-                if isinstance(v, dict):
-                    dest[k] = strip_none(v)
-                elif isinstance(v, list):
-                    dest[k] = []
-                    for e in v:
-                        dest[k].append(strip_none(e))
-                else:
-                    dest[k] = v
-        return dest
-    return source
