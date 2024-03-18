ad83124a801affdf84dc4056c7c17225035c2b5b
Maximilien Belinga <mbelinga@gitlab.com>
2024-03-18 03:40:51 +0000

    Update 2 files
    - /congregate/migration/gitlab/api/maven.py
    - /congregate/migration/gitlab/packages.py

---
+++ congregate/migration/gitlab/api/maven.py
@@ -0,0 +1,69 @@
+from urllib.parse import quote_plus
+from congregate.migration.gitlab.api.base_api import GitLabApiWrapper
+from congregate.migration.meta.api_models.npm_package_data import NpmPackageData
+
+class MavenPackagesApi(GitLabApiWrapper):
+    
+    def download_maven_project_package(self, host, token, pid, path, file_name):
+        """
+        Download a Maven project package file
+
+        GitLab API Doc: https://docs.gitlab.com/ee/api/packages/maven.html#download-a-package-file-at-the-project-level
+
+            :param host: (str) GitLab host URL
+            :param token: (str) Access token to GitLab instance
+            :param pid: (int) GitLab project ID
+            :param path: (str) The Maven package path, in the format <groupId>/<artifactId>/<version>. Replace any . in the groupId with /.
+            :param file_name: (str) The name of the Maven package file.
+            :return: Response object containing the response to GET projects/:id/packages/maven/*path/:file_name
+        """
+        return self.api.generate_get_request(host, token, f"projects/{pid}/packages/maven/{path}/{file_name}")
+
+    def download_maven_instance_package(self, host, token, path, file_name):
+        """
+        Download a Maven instance package file
+
+        GitLab API Doc: https://docs.gitlab.com/ee/api/packages/maven.html#download-a-package-file-at-the-instance-level
+
+            :param host: (str) GitLab host URL
+            :param token: (str) Access token to GitLab instance
+            :param path: (str) The Maven package path, in the format <groupId>/<artifactId>/<version>. Replace any . in the groupId with /.
+            :param file_name: (str) The name of the Maven package file.
+            :return: Response object containing the response to GET packages/maven/*path/:file_name
+        """
+        return self.api.generate_get_request(host, token, f"packages/maven/{path}/{file_name}")
+
+    def download_maven_group_package(self, host, token, pid, path, file_name):
+        """
+        Download a Maven group package file
+
+        GitLab API Doc: https://docs.gitlab.com/ee/api/packages/maven.html#download-a-package-file-at-the-group-level
+
+            :param host: (str) GitLab host URL
+            :param token: (str) Access token to GitLab instance
+            :param pid: (int) GitLab group ID
+            :param path: (str) The Maven package path, in the format <groupId>/<artifactId>/<version>. Replace any . in the groupId with /.
+            :param file_name: (str) The name of the Maven package file.
+            :return: Response object containing the response to GET groups/:id/-/packages/maven/*path/:file_name
+        """
+        return self.api.generate_get_request(host, token, f"groups/{pid}/-/packages/maven/{path}/{file_name}")
+    
+    def upload_maven_package(self, host, token, pid, path, files, file_name):
+        """
+        Upload a Maven package file
+
+        GitLab API Doc: https://docs.gitlab.com/ee/api/packages/maven.html#upload-a-package-file
+
+            :param host: (str) GitLab host URL
+            :param token: (str) Access token to GitLab instance
+            :param pid: (int) GitLab project ID
+            :param path: (str) The Maven package path, in the format <groupId>/<artifactId>/<version>. Replace any . in the groupId with /.
+            :param file_name: (str) The name of the Maven package file.
+            :return: Response object containing the response to PUT projects/:id/packages/maven/*path/:file_name
+        """
+        headers = {
+            'Private-Token': token,
+            'Content-Type': 'application/json'
+        }
+        
+        return self.api.generate_put_request(host, token, f"projects/{pid}/packages/maven/{path}/{file_name}", files=files, headers=headers)
--- congregate/migration/gitlab/packages.py
+++ congregate/migration/gitlab/packages.py
@@ -142,6 +142,51 @@ class PackagesClient(BaseClass):
         else:
             self.log.info(
                 f"Unable to find usable data (executable or pom file is missing) \n {package}")
+                
+    def migrate_maven_packages_v2(self, src_id, dest_id, package, project_name, results):
+        executable, pom_file, packaging = self.get_maven_files_data(
+            src_id, package)
+        if executable and pom_file:
+            artifact = self.format_artifact(
+                package['name'], package['version'])
+            self.log.info(
+                f"Attempting to download package: ({artifact}) from project: ({project_name})")
+            get_package_result = get_package(
+                projectName=project_name,
+                artifact=artifact,
+                remoteRepositories=f"gitlab-src::::{self.config.source_host}/api/v4/projects/{src_id}/packages/maven"
+            )
+            if get_package_result[0] == 0:
+                self.log.info(
+                    f"Package ({artifact}) was successfully downloaded. Moving on to deploying package")
+                deploy_package_result = deploy_package(
+                    projectName=project_name,
+                    groupId=self.format_groupid(package['name']),
+                    artifactId=self.format_artifactid(package['name']),
+                    version=package['version'],
+                    packaging=packaging,
+                    file=f"/opt/project_repositories/{project_name}/{package['name']}/{package['version']}/{executable.lower()}",
+                    pomFile=f"/opt/project_repositories/{project_name}/{package['name']}/{package['version']}/{pom_file}",
+                    repositoryId="gitlab-dest",
+                    url=f"{self.config.destination_host}/api/v4/projects/{dest_id}/packages/maven"
+                )
+                if deploy_package_result[0] == 0:
+                    self.log.info(
+                        f"Package ({artifact}) was successfully deployed")
+                    results.append({'Migrated': True, 'Package': artifact})
+
+                else:
+                    self.log.error(
+                        f"Package ({artifact}) was not successfully deployed")
+                    results.append({'Migrated': False, 'Package': artifact})
+                    self.log.error(deploy_package_result[1])
+            else:
+                self.log.error(
+                    f"Package ({artifact}) wasn't downloaded. Here is the stack trace \n {get_package_result[1]}")
+                results.append({'Migrated': False, 'Package': artifact})
+        else:
+            self.log.info(
+                f"Unable to find usable data (executable or pom file is missing) \n {package}")
 
     def migrate_pypi_packages(self, src_id, dest_id, package, results):
         version = package['version']
