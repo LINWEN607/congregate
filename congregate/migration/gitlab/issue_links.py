d36a9c0c444ad795ab69ab802e8d6112bae40607
mbelinga <mbelinga@gitlab.com>
2024-07-12 12:35:51 -0400

    Break down issues link migration logic

--- congregate/migration/gitlab/issue_links.py
+++ congregate/migration/gitlab/issue_links.py
@@ -14,33 +14,42 @@ class IssueLinksClient(BaseClass):
         """
         Migrate issue links from source projects to destination projects using project_id_mapping.
 
-        :param: project_id_mapping: (dict) Mapping of source project IDs to destination project IDs
+        :param project_id_mapping: (dict) Mapping of source project IDs to destination project IDs
         """
-        
         for src_project_id, dest_project_id in project_id_mapping.items():
-            for issue in self.issues_api.get_all_project_issues(src_project_id, self.config.source_host, self.config.source_token):
-                src_issue_iid = issue['iid']
-                # Get all issue links for the current issue
-                issue_links_response = self.issue_links_api.list_issue_links(self.config.source_host, self.config.source_token, src_project_id, src_issue_iid)
-                issue_links = issue_links_response.json()
-                for link in issue_links:
-                    if link:
-                        src_target_project_id = link['project_id']
-                        target_issue_iid = link['iid']
-                        link_type = link['link_type']
-                        if not self.dry_run:
-                            # Translate source project ID to destination project ID
-                            dst_target_project_id = project_id_mapping.get(str(src_target_project_id))
-                            if dst_target_project_id is None:
-                                self.log.info(f"Skipping link for issue {src_issue_iid}: unable to find destination ID for project {src_target_project_id}")
-                                continue
-                            # Recreate the issue link on the destination side
-                            self.issue_links_api.create_issue_link(
-                                self.config.destination_host,
-                                self.config.destination_token,
-                                dest_project_id,
-                                src_issue_iid,
-                                dst_target_project_id,
-                                target_issue_iid,
-                                link_type
-                            )
+            self.migrate_project_issue_links(src_project_id, dest_project_id, project_id_mapping)
+
+    def migrate_project_issue_links(self, src_project_id, dest_project_id, project_id_mapping):
+        issues = self.issues_api.get_all_project_issues(src_project_id, self.config.source_host, self.config.source_token)
+        for issue in issues:
+            self.migrate_issue_links_for_issue(issue, src_project_id, dest_project_id, project_id_mapping)
+
+    def migrate_issue_links_for_issue(self, issue, src_project_id, dest_project_id, project_id_mapping):
+        src_issue_iid = issue['iid']
+        issue_links = self.get_issue_links(src_project_id, src_issue_iid)
+        for link in issue_links:
+            self.migrate_single_issue_link(link, src_issue_iid, dest_project_id, project_id_mapping)
+
+    def get_issue_links(self, src_project_id, src_issue_iid):
+        issue_links_response = self.issue_links_api.list_issue_links(self.config.source_host, self.config.source_token, src_project_id, src_issue_iid)
+        return issue_links_response.json()
+
+    def migrate_single_issue_link(self, link, src_issue_iid, dest_project_id, project_id_mapping):
+        if link:
+            src_target_project_id = link['project_id']
+            target_issue_iid = link['iid']
+            link_type = link['link_type']
+            if not self.dry_run:
+                dst_target_project_id = project_id_mapping.get(int(src_target_project_id))
+                if dst_target_project_id is None:
+                    self.log.info(f"Skipping link for issue {src_issue_iid}: unable to find destination ID for project {src_target_project_id}")
+                    return
+                self.issue_links_api.create_issue_link(
+                    self.config.destination_host,
+                    self.config.destination_token,
+                    dest_project_id,
+                    src_issue_iid,
+                    dst_target_project_id,
+                    target_issue_iid,
+                    link_type
+                )
