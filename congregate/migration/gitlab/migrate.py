6405361ded7a8a4654f0d21b2299c13665445513
Petar Prokic <pprokic@gitlab.com>
2024-07-12 14:46:59 +0200

    Revert registry update

--- congregate/migration/gitlab/migrate.py
+++ congregate/migration/gitlab/migrate.py
@@ -661,10 +661,9 @@ class GitLabMigrateClient(MigrateClient):
                 src_id, dst_id, src_path)
 
             # Container Registries
-            if project.get("container_registry_enabled"):
-                if self.config.source_registry and self.config.destination_registry:
-                    results["container_registry"] = self.registries.migrate_registries(
-                        project)
+            if self.config.source_registry and self.config.destination_registry:
+                results["container_registry"] = self.registries.migrate_registries(
+                    project, dst_id)
 
             # Package Registries
             if project.get("packages_enabled"):
