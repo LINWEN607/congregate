2d86b7032b20a1d2f4e696c66ab7be658558ae24
Evgeny Rudinsky (GitLab) <erudinsky@gitlab.com>
2024-08-22 10:58:42 +0200

    Add remaining base classes to ADO base API classes, change default API version to stable (7.0)

--- congregate/helpers/conf.py
+++ congregate/helpers/conf.py
@@ -596,4 +596,4 @@ class Config(BaseConfig):
 
     @property
     def ado_api_version(self):
-        return self.prop("SOURCE", "api_version", default="7.1-preview.1")
+        return self.prop("SOURCE", "api_version", default="7.0")
--- congregate/migration/ado/api/base.py
+++ congregate/migration/ado/api/base.py
@@ -1,6 +1,14 @@
 import requests
-from gitlab_ps_utils.decorators import stable_retry
+
 from congregate.helpers.base_class import BaseClass
+from gitlab_ps_utils.decorators import stable_retry
+from gitlab_ps_utils.audit_logger import audit_logger
+from gitlab_ps_utils.logger import myLogger
+from gitlab_ps_utils.misc_utils import generate_audit_log_message, safe_json_response
+
+log = myLogger(__name__)
+audit = audit_logger(__name__)
+
 
 class AzureDevOpsClient(BaseClass):
 
@@ -14,7 +22,7 @@ class AzureDevOpsClient(BaseClass):
         return f"{self.config.source_host}/{api}"
     
     @stable_retry
-    def generate_get_request(self, api, params=None):
+    def generate_get_request(self, api, params=None, description=None):
         """
         Generates GET request to ADO API.
         You will need to provide the access token, and specific api url.
@@ -27,10 +35,111 @@ class AzureDevOpsClient(BaseClass):
         """
 
         url = self.generate_request_url(api)
+        audit.info(generate_audit_log_message("GET", description, url))
         headers = self._get_headers()
         if params:
             params['api-version'] = self.config.ado_api_version
         else:
             params = {'api-version': self.config.ado_api_version}
-
         return requests.get(url, params=(params or {}), headers=headers, verify=self.config.ssl_verify)
+
+    @stable_retry
+    def generate_post_request(self, api, data, description=None):
+        """
+        Generates POST request to ADO API.
+        You will need to provide the access token, and specific api url.
+
+            :param token: (str) Access token to ADO instance
+            :param api: (str) Specific ADO API endpoint (ex: projects)
+            :param url: (str) A URL to a location not part of the ADO API. Defaults to None
+            :param data: (dict) Data to be posted
+            :return: The response object *not* the json() or text()
+        """
+
+        url = self.generate_request_url(api)
+        audit.info(generate_audit_log_message("POST", description, url))
+
+        headers = self._get_headers()
+        headers['Content-Type'] = 'application/json'
+        if params:
+            params['api-version'] = self.config.ado_api_version
+        else:
+            params = {'api-version': self.config.ado_api_version}
+
+        return requests.post(url, data=data, headers=headers, verify=self.config.ssl_verify)
+
+    @stable_retry
+    def generate_patch_request(self, api, data, description=None):
+        """
+        Generates PATCH request to ADO API.
+        You will need to provide the access token, and specific api url.
+
+            :param token: (str) Access token to ADO instance
+            :param api: (str) Specific ADO API endpoint (ex: projects)
+            :param url: (str) A URL to a location not part of the ADO API. Defaults to None
+            :param data: (dict) Data to be posted
+            :return: The response object *not* the json() or text()
+        """
+
+        url = self.generate_request_url(api)
+        audit.info(generate_audit_log_message("PATCH", description, url))
+        headers = self._get_headers()
+        return requests.patch(url, data=data, headers=headers, verify=self.config.ssl_verify)
+
+    @stable_retry
+    def generate_put_request(self, api, data, description=None):
+        """
+        Generates PUT request to ADO API.
+        You will need to provide the access token, and specific api url.
+
+            :param token: (str) Access token to ADO instance
+            :param api: (str)
+            :param url: (str) A URL to a location not part of the ADO API. Defaults to None
+            :param data: (dict) Data to be posted
+            :return: The response object *not* the json() or text()
+        """
+
+        url = self.generate_request_url(api)
+        audit.info(generate_audit_log_message("PUT", description, url))
+        headers = self._get_headers()
+        return requests.put(url, data=data, headers=headers, verify=self.config.ssl_verify)
+
+    @stable_retry
+    def generate_delete_request(self, api, description=None):
+        """
+        Generates DELETE request to ADO API.
+        You will need to provide the access token, and specific api url.
+
+            :param token: (str) Access token to ADO instance
+            :param api: (str) Specific ADO API endpoint (ex: projects)
+            :param url: (str) A URL to a location not part of the ADO API. Defaults to None
+            :return: The response object *not* the json() or text()
+        """
+
+        url = self.generate_request_url(api)
+        audit.info(generate_audit_log_message("DELETE", description, url))
+        headers = self._get_headers()
+        return requests.delete(url, headers=headers, verify=self.config.ssl_verify)
+
+
+    def list_all(self, api, params=None):
+        """
+        Generates a list of all projects, groups, users, etc.
+
+            :param api: (str) Specific ADO API endpoint (ex: users)
+            :param params: (str) Any query parameters needed in the request
+            :yields: Individual objects from the presumed array of data    
+        """
+
+        while True:
+            response = self.generate_get_request(api, params=params)
+            response.raise_for_status()
+            data = safe_json_response(response)
+            for item in data.get("value", []):
+                yield item
+
+            if "x-ms-continuationtoken" not in response.headers:
+                break
+
+            params["continuationToken"] = response.headers["x-ms-continuationtoken"]
+
