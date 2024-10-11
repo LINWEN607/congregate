ff4e0fd6042aa4b2af20844826c71bd15b64eac3
Michael Leopard <mleopard@gitlab.com>
2024-10-11 09:17:40 -0400

    Switching to StageTable component for users and groups

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,120 +1,101 @@
 <template>
-  <div class="users">
-    <h2>Stage Users</h2>
-    <div class="table">
-        <vue-good-table
-            ref="users-table"
-            id="users-table"
-            :columns="columns"
-            :rows="rows"
-            :search-options="{ enabled: true }"
-            :select-options="{ enabled: true }"
-            :pagination-options="{ enabled: true, perPage: 25 }"
-            :line-numbers="true"
-        />
-    </div>
-    <Footer msg="Stage" asset="stage-users"/>
-  </div>
+  <StageTable asset="users" :columns="columns"/>
 </template>
 
 <script>
-import 'vue-good-table/dist/vue-good-table.css'
-import { VueGoodTable } from 'vue-good-table'
-import axios from 'axios'
-import Footer from '@/components/Footer.vue'
+import StageTable from '@/components/StageTable.vue'
 
 export default {
   name: 'Users',
   components: {
-    VueGoodTable,
-    Footer
+    StageTable
   },
   data () {
     return {
       columns: [
         {
-          label: 'ID',
+          title: 'ID',
           field: 'id',
           type: 'number'
         },
         {
-          label: 'Name',
+          title: 'Name',
           field: 'name'
         },
         {
-          label: 'Username',
+          title: 'Username',
           field: 'username'
         },
         {
-          label: 'Email',
+          title: 'Email',
           field: 'email'
         },
         {
-          label: 'State',
+          title: 'State',
           field: 'state'
         }
       ],
       rows: []
     }
   },
-  mounted: function () {
-    this.getData()
-    this.$emitter.on('stage-users', event => {
-      this.stageData()
-    })
-  },
-  beforeDestroy: function() {
-    this.$emitter.off('stage-users')
-  },
-  methods: {
-    getData: function () {
-      axios.get(`${import.meta.env.VITE_API_ROOT}/api/data/users`).then(response => {
-        this.rows = response.data
-        this.getStagedData()
-      }).catch(function (error) {
-        console.log(error)
-      })
-    },
-    getStagedData: function () {
-      axios.get(`${import.meta.env.VITE_API_ROOT}/api/data/staged_users`).then(response => {
-        let ids = []
-        response.data.forEach(element => {
-          ids.push(element.id)
-        })
-        console.log(ids)
-        let table = this.$refs['users-table']
-        this.$refs['users-table'].rows.forEach((element, ind) => {
+  // mounted: function () {
+  //   this.getData()
+  //   this.$emitter.on('stage-users', event => {
+  //     this.stageData()
+  //   })
+  // },
+  // beforeDestroy: function() {
+  //   this.$emitter.off('stage-users')
+  // },
+  // methods: {
+  //   getData: function () {
+  //     axios.get(`${import.meta.env.VITE_API_ROOT}/api/data/users`).then(response => {
+  //       this.rows = response.data
+  //       this.getStagedData()
+  //     }).catch(function (error) {
+  //       console.log(error)
+  //     })
+  //   },
+  //   getStagedData: function () {
+  //     axios.get(`${import.meta.env.VITE_API_ROOT}/api/data/staged_users`).then(response => {
+  //       let ids = []
+  //       response.data.forEach(element => {
+  //         ids.push(element.id)
+  //       })
+  //       console.log(ids)
+  //       let table = this.$refs['users-table']
+  //       this.$refs['users-table'].rows.forEach((element, ind) => {
           
-          if (ids.includes(element.id)) {
-            console.log("updating user table")
-            table.$set(table.rows[ind], 'vgtSelected', true)
-          }
-        })
-      }).catch(function (error) {
-        console.log(error)
-      })
-    },
-    stageData: function () {
-      if (this.$refs['users-table'].selectedRows) {
-        let usernames = []
-        this.$refs['users-table'].selectedRows.forEach(element => {
-          usernames.push(element.username)
-        })
-        axios.post(`${import.meta.env.VITE_API_ROOT}/api/stage/users`, String(usernames)).then(response => {
-          console.log(response)
-          this.$emitter.emit('alert', {
-            'message': response.data,
-            'messageType': 'done'
-          })
-        }).catch(response => {
-          console.log(response)
-          this.$emitter.emit('alert', {
-            'message': 'Unable to stage users',
-            'messageType': 'error'
-          })
-        })
-      }
-    }
-  }
+  //         if (ids.includes(element.id)) {
+  //           console.log("updating user table")
+  //           table.$set(table.rows[ind], 'vgtSelected', true)
+  //         }
+  //       })
+  //     }).catch(function (error) {
+  //       console.log(error)
+  //     })
+  //   },
+  //   stageData: function () {
+  //     if (this.$refs['users-table'].selectedRows) {
+  //       let usernames = []
+  //       this.$refs['users-table'].selectedRows.forEach(element => {
+  //         usernames.push(element.username)
+  //       })
+  //       axios.post(`${import.meta.env.VITE_API_ROOT}/api/stage/users`, String(usernames)).then(response => {
+  //         console.log(response)
+  //         this.$emitter.emit('alert', {
+  //           'message': response.data,
+  //           'messageType': 'done'
+  //         })
+  //       }).catch(response => {
+  //         console.log(response)
+  //         this.$emitter.emit('alert', {
+  //           'message': 'Unable to stage users',
+  //           'messageType': 'error'
+  //         })
+  //       })
+  //     }
+  //   }
+  // }
 }
 </script>
