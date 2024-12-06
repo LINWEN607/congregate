51d2f311e3f46626e9871a86f80cc14556ed924e
Petar Prokic <pprokic@gitlab.com>
2024-12-06 16:15:09 +0100

    Add default location

--- congregate/helpers/conf.py
+++ congregate/helpers/conf.py
@@ -270,7 +270,7 @@ class Config(BaseConfig):
 # EXPORT
     @property
     def location(self):
-        return self.prop("EXPORT", "location")
+        return self.prop("EXPORT", "location", default="filesystem")
 
     @property
     def bucket_name(self):
