53bd54d7267e6c5cc689afe7f3344580a3accca4
Michael Leopard <mleopard@gitlab.com>
2025-01-17 12:30:54 -0500

    Updating e2e test instance type again

--- ci/spin_up_test_vm.sh
+++ ci/spin_up_test_vm.sh
@@ -4,7 +4,7 @@ spin_up_instance () {
     poetry run aws ec2 run-instances \
         --image-id $AMI_ID \
         --count 1 \
-        --instance-type c5a.xlarge \
+        --instance-type c5a.2xlarge \
         --key-name $KEY_NAME \
         --security-group-ids $SECURITY_GROUP
 }
