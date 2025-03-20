49fef2081944ecd24c24ae7a919631171cdf8c7f
Petar Prokic <pprokic@gitlab.com>
2025-03-20 12:25:27 +0100

    Fix FF feature return

--- congregate/migration/gitlab/migrate.py
+++ congregate/migration/gitlab/migrate.py
@@ -679,9 +679,7 @@ class GitLabMigrateClient(MigrateClient):
             # Project Feature Flag Users Lists
             project_feature_flags_users_lists = self.project_feature_flags_users_lists_client.migrate_project_feature_flags_user_lists_for_project(
                 src_id, dst_id)
-            if project_feature_flags_users_lists:
-                results["project_feature_flags_users_lists"] = project_feature_flags_users_lists.get(
-                    'completed')
+            results["project_feature_flags_users_lists"] = project_feature_flags_users_lists.get('completed')
 
             # Project Feature Flags
             results["project_feature_flags"] = self.project_feature_flags_client.migrate_project_feature_flags_for_project(
